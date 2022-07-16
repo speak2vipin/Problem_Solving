12983d72926c777ec92b6c3b907429eaa248ddbd
Vipin Kumar Gupta <65086274+speak2vipin8@users.noreply.github.com>
2022-07-16 23:55:16 +0530

    Time: 4 ms (30.80%), Space: 42 MB (63.49%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -8,7 +8,7 @@ class Solution {
 		for (int i = 0; i < strs[0].length(); i++) {
 			String temp = "" + strs[0].charAt(i);
 			for (int j = 1; j < strs.length; j++) {
-				if (i<strs[j].length() && strs[j].charAt(i) == strs[0].charAt(i) && strs[j].contains(result + temp)) {
+				if (i<strs[j].length() && strs[j].charAt(i) == strs[0].charAt(i)) {
 					if (j == strs.length - 1) {
 						if (result == null) {
 							result = temp;
