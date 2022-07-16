4d5c644826d7660b805e13ebe4447c7fb2cb1046
Vipin Kumar Gupta <65086274+speak2vipin8@users.noreply.github.com>
2022-07-17 00:08:33 +0530

    Time: 4 ms (30.80%), Space: 42.6 MB (24.79%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -3,22 +3,25 @@ class Solution {
         if(strs.length==1) {
             return strs[0];
         }
-        char temp;
         String result = "";
+        String temp = null;         
+
 		for (int i = 0; i < strs[0].length(); i++) {
-		    temp = strs[0].charAt(i);	
+			temp = "" + strs[0].charAt(i);
 			for (int j = 1; j < strs.length; j++) {
-				if (i<strs[j].length() && strs[j].charAt(i) == temp) {
+				if (i<strs[j].length() && strs[j].charAt(i) == strs[0].charAt(i)) {
 					if (j == strs.length - 1) {
-						result = result + temp;
+						if (result == null) {
+							result = temp;
+						} else {
+							result = result + temp;
+						}
 					}
 				} else {
 					return result;
 				}
 			}
 		}
-
 		return result;
-
     }
 }
