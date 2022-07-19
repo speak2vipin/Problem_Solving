6ba7b0b0120f8765c3e5c8b57a8a3a718f71ef35
Vipin Kumar Gupta <65086274+speak2vipin8@users.noreply.github.com>
2022-07-19 17:22:27 +0530

    Time: 1 ms (100.00%), Space: 46.6 MB (42.47%) - LeetHub

--- 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.java
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.java
@@ -2,7 +2,7 @@ class Solution {
     public int smallestEqual(int[] nums) {
         for(int i=0; i<nums.length; i++) {
             if(nums[i]>=10) {
-                
+                continue;
             }else {
                 if(i%10==nums[i]){
                     return i;
