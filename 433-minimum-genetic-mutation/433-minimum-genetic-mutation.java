d946dc19ca2326d1b3d7a8fcc74954eda10acc1e
Vipin Kumar Gupta <65086274+speak2vipin8@users.noreply.github.com>
2022-07-24 15:58:22 +0530

    Time: 3 ms (17.68%), Space: 42.2 MB (31.12%) - LeetHub

---
+++ 433-minimum-genetic-mutation/433-minimum-genetic-mutation.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int minMutation(String start, String end, String[] bank) {
+        
+		char genes[] = { 'A', 'C', 'G', 'T' };
+		if (start.equals(end)) {
+			return 0;
+		} else if (bank.length == 0) {
+			return -1;
+		}
+		
+		int level = 0;
+		List<String> bankString = new ArrayList<String>();
+		for(String b : bank) {
+			bankString.add(b);
+		}
+		Queue<String> sList = new LinkedList<String>();
+		Set<String>visited = new HashSet<String> ();
+		sList.offer(start);
+		while (!sList.isEmpty()) {
+			int size = sList.size();
+			while (size-- > 0) {
+				
+				String s = sList.poll();		//AAAAACCC || AAAACCCC","AAACCCCC","AACCCCCC" || AACCCCCC
+				if(s.equals(end)) {
+					return level;
+				}
+				for (int i = 0; i < s.length(); i++) {
+					char[] charArray = s.toCharArray();
+					for (int j = 0; j < genes.length; j++) {
+						charArray[i] = genes[j];
+						String temp = new String(charArray);
+						if (visited.add(temp) && bankString.contains(temp)) {
+							sList.offer(temp);
+							//visited.add(temp);
+						}
+
+					}
+				}
+			}
+			if(!sList.isEmpty())
+				level++;
+		}
+		return -1;
+	
+     
+    }
+}
