37098d798de9ee2e15a0863e5f9130e3f86794e9
Vipin Kumar Gupta <65086274+speak2vipin8@users.noreply.github.com>
2022-08-02 23:06:43 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (55.37%) - LeetHub

---
+++ 66-plus-one/66-plus-one.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        boolean isZero =false;
+        for(int i=digits.length-1;i>=0;i--) {
+            if(digits[i] + 1 == 10) {
+                isZero = true;
+                digits[i] = 0;
+            } else {
+                isZero = false;
+                digits[i] = digits[i] + 1;
+                break;
+            }
+        }
+        if(isZero) {
+            int res[] = new int[digits.length+1];
+            res[0] = 1;
+            return res;
+        }
+        return digits;
+    }
+}
