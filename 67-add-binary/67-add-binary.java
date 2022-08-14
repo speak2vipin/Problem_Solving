eaca8f9349bbda6d84c4071615591dc08735f916
Vipin Kumar Gupta <65086274+speak2vipin8@users.noreply.github.com>
2022-08-15 00:02:26 +0530

    Time: 7 ms (25.68%), Space: 43 MB (47.24%) - LeetHub

--- 67-add-binary/67-add-binary.java
+++ 67-add-binary/67-add-binary.java
@@ -1,6 +1,6 @@
 class Solution {
-    public String addBinary(String a, String b) {
-        
+    public String addBinary(String A, String B) {
+        /**
         StringBuilder sb = new StringBuilder();
         int i = a.length() - 1, j = b.length() -1, carry = 0;
         while (i >= 0 || j >= 0) {
@@ -12,5 +12,72 @@ class Solution {
         }
         if (carry != 0) sb.append(carry);
         return sb.reverse().toString();
+        **/
+        int aLen = A.length();
+		int bLen = B.length();
+		String Ans = "";
+		char carry = '0';
+		String minTemp;
+		String maxTemp;
+		if(aLen>bLen) {
+			minTemp = B;
+			maxTemp = A;
+		} else {
+			minTemp = A;
+			maxTemp = B;
+		}
+		
+		int i,k;
+		for (i = minTemp.length()-1, k = maxTemp.length()-1 ; i > -1; i--, k--) {
+			if (minTemp.charAt(i) == '0' && maxTemp.charAt(k) == '0') {
+				if (carry == '0') {
+					Ans = '0' + Ans;
+				} else {
+					Ans = '1' + Ans;
+					carry = '0';
+				}
+			} else if (minTemp.charAt(i) == '0' && maxTemp.charAt(k) == '1') {
+				if (carry == '0') {
+					Ans = '1' + Ans;
+				} else {
+					Ans = '0' + Ans;
+					carry = '1';
+				}
+			} else if (minTemp.charAt(i) == '1' && maxTemp.charAt(k) == '0') {
+				if (carry == '0') {
+					Ans = '1' + Ans;
+				} else {
+					Ans = '0' + Ans;
+					carry = '1';
+				}
+			} else if (minTemp.charAt(i) == '1' && maxTemp.charAt(k) == '1') {
+				if (carry == '0') {
+					Ans = '0' + Ans;
+					carry = '1';
+				} else {
+					Ans = '1' + Ans;
+					carry = '1';
+				}
+			}
+		}
+		
+		for (; k > -1; k--) {
+				if (maxTemp.charAt(k) == '0' && carry == '0') {
+					Ans = '0' + Ans;
+				} else if (maxTemp.charAt(k) == '1' && carry == '0') {
+					Ans = '1' + Ans;
+				} else if (maxTemp.charAt(k) == '0' && carry == '1') {
+					Ans = '1' + Ans;
+					carry = '0';
+				} else if (maxTemp.charAt(k) == '1' && carry == '1') {
+					Ans = '0' + Ans;
+					carry = '1';
+				}
+		}
+		if (carry == '1') {
+			Ans = '1' + Ans;
+		}
+		return Ans;	
+    
     }
 }
