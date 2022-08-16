f236a5f557c99cdb3e6f12dc7a19e12b2ce83617
Vipin Kumar Gupta <65086274+speak2vipin8@users.noreply.github.com>
2022-08-16 13:15:34 +0530

    Time: 7 ms (93.77%), Space: 50.1 MB (48.86%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public int firstUniqChar(String s) {  
-      /*  int index[] = new int[26];
+        int index[] = new int[26];
         char[] sArray = s.toCharArray();
         for(char temp : sArray) {
             index[temp-'a'] += 1; 
@@ -10,12 +10,12 @@ class Solution {
                 return s.indexOf(temp);
             }
         }
-        */
-        for(int i=0; i<s.length();i++) {
+        
+       /* for(int i=0; i<s.length();i++) {
             if(s.indexOf(s.charAt(i))==s.lastIndexOf(s.charAt(i))) {
         	     return i;	
         	}
-        }
+        }*/
         return -1; 
     }
 }
