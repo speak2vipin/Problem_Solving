45bcb3fe8136428ccb588fcffb3d42aac31aa149
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2022-08-29 00:25:19 +0530

    Time: 7 ms (80.21%), Space: 42.8 MB (99.22%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -1,16 +1,47 @@
 class Solution {
-    public int[][] diagonalSort(int[][] A) {
-       int m = A.length, n = A[0].length;
-        HashMap<Integer, PriorityQueue<Integer>> d = new HashMap<>();
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                d.putIfAbsent(i - j, new PriorityQueue<>());
-                d.get(i - j).add(A[i][j]);
-            }
-        }
-        for (int i = 0; i < m; ++i)
-            for (int j = 0; j < n; ++j)
-                A[i][j] = d.get(i - j).poll();
-        return A;
+    public int[][] diagonalSort(int[][] a) {
+	        int M = a.length;
+	        int N = a[0].length;
+	        for(int j=N-1;j>-1;j--) {
+	            int i=0, k=j;
+	            List<Integer> list = new ArrayList<Integer>();
+	            while(i<M && k<N) {
+	                list.add(a[i][k]);
+	                i++;
+	                k++;
+	            }
+	            Collections.sort(list);
+	            i=0;
+	            k=j;
+	            int index = 0;
+	            while(i<M && k<N && index<list.size()) {
+	                a[i][k] = list.get(index);
+	                i++;
+	                k++;
+	                index++;
+	            }    
+	        }
+	        
+	        for(int i=1;i<M;i++) {
+	            int j=0, k=i;
+	            List<Integer> list = new ArrayList<Integer>();
+	            while(k<M && j<N) {
+	                list.add(a[k][j]);
+	                k++;
+	                j++;
+	            }
+	            Collections.sort(list);
+	            k=i;
+	            j=0;
+	            int index = 0;
+	            while(k<M && j<N && index<list.size()) {
+	                a[k][j] = list.get(index);
+	                k++;
+	                j++;
+	                index++;
+	            }    
+	        }
+	        return a;
+	    
     }
 }
