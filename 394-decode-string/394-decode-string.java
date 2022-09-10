09ba96122b1b2af46c5148ce64f55d29bab28c50
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2022-09-11 00:06:38 +0530

    Time: 10 ms (12.05%), Space: 42.5 MB (29.18%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -1,27 +1,39 @@
 class Solution {
    public String decodeString(String s) {
-         Deque<Character> queue = new LinkedList<>();
-        for (char c : s.toCharArray()) queue.offer(c);
-        return helper(queue);
-    }
-    
-    public String helper(Deque<Character> queue) {
-        StringBuilder sb = new StringBuilder();
-        int num = 0;
-        while (!queue.isEmpty()) {
-            char c= queue.poll();
-            if (Character.isDigit(c)) {
-                num = num * 10 + c - '0';
-            } else if (c == '[') {
-                String sub = helper(queue);
-                for (int i = 0; i < num; i++) sb.append(sub);   
-                num = 0;
-            } else if (c == ']') {
-                break;
-            } else {
-                sb.append(c);
-            }
-        }
-        return sb.toString();
-    }
+      
+		while (s.lastIndexOf('[') != -1) {
+			int i = s.lastIndexOf('[');
+			int count = 0;
+			char c = s.charAt(i - 1);
+			int tempCount = 0;
+
+			while (c > 47 && c < 58) {
+				
+				tempCount += (c - '0') * Math.pow(10, count);
+				count++;
+				if(i - 1 - count>-1)
+					c = s.charAt(i - 1 - count);
+				else
+					break;
+			}
+			int k = i + mod(s.substring(i + 1));
+			s = s.substring(0, i - count) + recurString(s.substring(i + 1, k), tempCount, "") + s.substring(k+1);
+		}
+		return s;
+	}
+
+	  String recurString(String s, int A, String temp) {
+		if (A == 1) {
+			return temp + s;
+		}
+		return recurString(s, A - 1, temp + s);
+	}
+
+	 int mod(String s) {
+		int i = 0;
+		while (s.charAt(i) != ']') {
+			i++;
+		}
+		return i+1;
+	}
 }
