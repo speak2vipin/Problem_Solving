941ac03c3013983c77d53afc6702e3b15cc0056b
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2022-09-12 00:12:11 +0530

    Time: 120 ms (15.00%), Space: 69 MB (49.67%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
-      /*  int N = speed.length;
+        int N = speed.length;
         int [][]temp = new int[N][2];
         for(int i=0; i<N; i++) {
             temp[i] = new int[] {efficiency[i], speed[i]};
@@ -9,8 +9,8 @@ class Solution {
         Arrays.sort(temp, (a,b) -> b[0] - a[0]);
         // Default order i.e. Ascending order
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-        int sum = 0;
-        int netEfficiency = 0;
+        long sum = 0;
+        long netEfficiency = 0;
         for(int i=0; i<N; i++) {
             netEfficiency+=temp[i][1];
             pq.add(temp[i][1]);
@@ -20,8 +20,9 @@ class Solution {
             // Trick is here i.e if in Queue, didn't get higher number than              //netEfficiency will not change and we knows A[0] would be in descending order so it's //multiply will not be higher than it's previos value for sure.
             sum = Math.max(sum, netEfficiency*temp[i][0]);
         }
-        return (int) (sum % (long) (1e9 + 7));*/
-        int[][] ess = new int[n][2];
+        return (int) (sum % (long) (1e9 + 7));
+    }
+       /* int[][] ess = new int[n][2];
 		for (int i = 0; i < n; ++i)
 			ess[i] = new int[] { efficiency[i], speed[i] };
 		Arrays.sort(ess, (a, b) -> b[0] - a[0]);
@@ -35,5 +36,5 @@ class Solution {
 			res = Math.max(res, (sumS * es[0]));
 		}
 		return (int) (res % (long) (1e9 + 7));
-    }
+    }*/
 }
