f5677af388c832a991663b1983ceb4117644f33d
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-01-18 20:19:11 +0530

    Time: 5 ms (60.60%), Space: 49.8 MB (51.72%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
@@ -5,28 +5,33 @@ class Solution {
         int rightMax[] = new int[N];
         rightMax[N-1] = nums[N-1];
         int suffixSum = nums[N-1];
+        // Find max possible sum from right hand side
+        
         for(int i=N-2; i>-1; i--) {
             suffixSum += nums[i];
             rightMax[i] = Math.max(rightMax[i+1], suffixSum);
         }
-        int prefix[] = new int[N];
-        prefix[0] = nums[0];
+        
         int maxSum = nums[0];
+        // Special Sum would be sum with rotated elements
         int specialSum = Integer.MIN_VALUE;
         int prefixSum = 0;
         int curMax = 0;
-        int max = Integer.MIN_VALUE;
+   
         for(int i=0; i<N; i++) {
+            // Simple Kadane algorithm
             if(curMax + nums[i]>0) {
                 curMax = curMax + nums[i];
                 maxSum = Math.max(maxSum, curMax);
             } else {
                 curMax = 0;
             } 
-            max = Math.max(max, nums[i]);
+            // Considering num[i] for being max
             maxSum = Math.max(maxSum, nums[i]);
+            // Prefix Sum for calculating special sum i.e. rotated array sum
             prefixSum += nums[i];
-            if(i<N-2) {
+            
+            if(i<N-1) {
                 specialSum = Math.max(specialSum, prefixSum + rightMax[i+1]);
             }
             
