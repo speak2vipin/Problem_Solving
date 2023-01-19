57c3f9e4340b9be53bd8d5610c1b24a994e929c9
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-01-19 12:45:29 +0530

    Time: 3 ms (98.01%), Space: 43.5 MB (99.94%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -5,22 +5,24 @@ class Solution {
         int prefixMod[] = new int[N];
         int prefix = 0;
         int remainder = 0;
+        // Evaluate remainder prefixSum array
         for(int i=0; i<N; i++) {
             prefix+=nums[i];
             remainder = prefix%k;     
             if(remainder < 0) {
             	remainder = (remainder+k)%k;
             }
-            prefixMod[i] = remainder;
-            
+            prefixMod[i] = remainder;   
         }
         int count=0;
         int modgroup[] = new int[k];
-
+        
         for(int i=0; i<N; i++) {
+            // If prefixMod is zero means that subArray has sum which is divisible 
             if(prefixMod[i]==0) {
                 count++;
             }
+            // Check if we have same remainder i.e. remainder diff between those two index would be 0
             count += modgroup[prefixMod[i]];
             modgroup[prefixMod[i]] = modgroup[prefixMod[i]]+1;
         }
