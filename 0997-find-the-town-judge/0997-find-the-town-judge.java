6060e4e2c32b84af0b17012ee212ec290755e94a
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-01-23 13:00:54 +0530

    Time: 17 ms (27.12%), Space: 73.9 MB (17.59%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,5 +1,7 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
+        /*boolean[] people = new boolean[n];
+        Arrays.fill(people, true);
         int ans = -1;
         if(trust.length==0) {
             if(n==1) {
@@ -21,5 +23,26 @@ class Solution {
             }
         }
         return ans;
+        */
+        
+		boolean[] people = new boolean[n+1];
+        int[]count = new int[n+1];
+		Arrays.fill(people, true);
+		int ans = -1;
+
+		for (int[] t : trust) {
+			people[t[0]] = false;
+            count[t[1]] = count[t[1]]+1;
+		}
+
+		for (int i = 1; i < n+1; i++) {
+			if (people[i] && count[i]==n-1) {
+					ans = i;
+				} 
+		}
+
+		
+		return ans;
+	
     }
 }
