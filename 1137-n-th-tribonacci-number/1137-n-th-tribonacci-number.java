bf60fcef22015e7caacdd31f2679cb3db9df6aed
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-01-30 12:45:43 +0530

    Time: 0 ms (100.00%), Space: 38.7 MB (95.83%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,14 +1,31 @@
 class Solution { 
     
+
+//    public int tribonacci(int n) {
+ // Gives TLE, intention is to check the basics of DP
+ //      if(n<=1) {
+ //           return n;
+ //       } else if(n==2) {
+ //          return 1;
+ //       }
+ //      return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+ //
+ //       if(n<=1) {
+ //         return n;
+ //       } else if(n==2) {
+ //           return 1;
+ //       }
+ //       int f[] = new int[n+1];
+ //       f[0] = 0;
+ //       f[1] = 1;
+ //       f[2] = 1;
+ //       for(int i=3; i<n+1; i++) {
+ //           f[i] = f[i-1]+f[i-2]+f[i-3];
+ //       }
+ //       return f[n];
+ //  }
+    
     public int tribonacci(int n) {
-      /* if(n<=1) {
-            return n;
-        } else if(n==2) {
-            return 1;
-        }
-        return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
-        */
-            
     
         int f[] = new int[n+1];
         if(n<=1) 
