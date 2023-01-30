90cfe43094a2a10cd845fa4aed8cb3f9658d6d0a
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-01-30 12:36:34 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (61.90%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,4 +1,5 @@
-class Solution {
+class Solution { 
+    
     public int tribonacci(int n) {
       /* if(n<=1) {
             return n;
@@ -7,18 +8,29 @@ class Solution {
         }
         return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
         */
-         if(n<=1) {
-            return n;
-        } else if(n==2) {
-            return 1;
-        }
+            
+    
         int f[] = new int[n+1];
+        if(n<=1) 
+            return n;
+        else if(n==2)
+            return 1;
+        
         f[0] = 0;
         f[1] = 1;
         f[2] = 1;
-        for(int i=3; i<n+1; i++) {
-            f[i] = f[i-1]+f[i-2]+f[i-3];
-        }
-        return f[n];
+       
+        return helper(n, f);
+    }
+    
+    int helper(int n, int f[]) {
+        if(n==0 || f[n]>0) 
+            return f[n];
+        
+        f[n-1] = helper(n-1, f);
+        f[n-2] = helper(n-2, f);
+        f[n-3] = helper(n-3, f);
+            
+        return f[n-1]+f[n-2]+f[n-3];
     }
 }
