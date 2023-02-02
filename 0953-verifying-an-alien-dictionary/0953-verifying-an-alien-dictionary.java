ec44536b543726a1f55dabdc5aadb0008e19c3f2
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-02-02 13:43:16 +0530

    Time: 1 ms (65.29%), Space: 42.3 MB (44.58%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
@@ -0,0 +1,43 @@
+class Solution {
+    Map<Character, Integer> dic = new HashMap<Character, Integer>();
+    
+    public boolean isAlienSorted(String[] words, String order) {
+        
+        int l = order.length();
+        for(int i=0; i<l; i++) {
+            dic.put(order.charAt(i), i);
+        }
+        for(int i=1; i<words.length; i++) {
+            if(!compare(words[i-1], words[i])) {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+    
+    boolean compare(String s1, String s2) {
+        int l1 = s1.length()-1;
+        int l2 = s2.length()-1;
+        int l = l1 > l2 ? l2 : l1;
+        char c1 = '\u0000';
+        char c2 = '\u0000';
+        for(int i=0; i<l+1; i++) {
+            c1 = s1.charAt(i);
+            c2 = s2.charAt(i);
+            if(c1==c2) {
+               continue;
+            } 
+            if(dic.get(c1)>dic.get(c2)) {
+                return false;
+            }
+            if(dic.get(c1)<dic.get(c2)) {
+                return true;
+            }
+        }
+        if(l1>l2) {
+            return false;
+        }
+        return true;
+    }
+}
