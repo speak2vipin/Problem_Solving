b708d6db8234ddd2fdfc9fb5f9ec7bc9447e78b9
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-02-03 20:30:08 +0530

    Time: 16 ms (49.04%), Space: 42.9 MB (67.65%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String convert(String s, int numRows) {
+         int l = s.length();
+        if(l<numRows || numRows==1) {
+            return s;
+        }
+        String output = "";
+        ArrayList<ArrayList<Character>> A = new ArrayList<ArrayList<Character>>();
+        int i=0;
+        for(i=0; i<numRows; i++) {
+            ArrayList<Character> temp = new ArrayList<Character>();
+            temp.add(s.charAt(i));
+            A.add(temp);
+        }
+        boolean flag = true;
+        int j=1;
+
+        while(i<l) { 
+            if(flag) {
+                A.get(numRows-1-j).add(s.charAt(i));
+            } else {
+                A.get(j).add(s.charAt(i));
+            }
+            
+            if(j == numRows-1) {
+                j=1;
+                flag = !flag;  
+            } else {
+            	j++;
+            }
+            i++;
+            
+        }
+        for(ArrayList<Character> list : A) {
+            for(Character c : list) {
+                output += c;
+            }
+        }
+        return output;
+    }
+}
