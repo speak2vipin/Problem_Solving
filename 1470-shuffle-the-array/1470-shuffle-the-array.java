8f63a5dc3cd9b6cd392c4598fac81b5147b20426
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-02-06 13:27:14 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (69.16%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,10 +1,21 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {      
-        int ans[] = new int[2*n];
-        for(int i=0; i<n; i++) {
-            ans[2*i] = nums[i];
-            ans[2*i+1] = nums[n+i];
+              
+        // 1 <= nums[i] <= 10^3 i.e 1000 would be within 11111 11111 in binary 
+        // i.e. Not more than that.
+        for(int i=n; i<2*n; i++) {
+        	int temp = nums[i]<<10;		// Number will left shift to 10 digits
+            nums[i-n] = nums[i-n] | temp;	// Or'ed it with nums[i-n] 
         }
-        return ans;
-    }
+        int calculateFactors = (int)Math.pow(2, 10)-1;	
+        // This would be 00000 00000 11111 11111
+        for(int i=n-1; i>-1; i--) {
+        	int first = nums[i] & (calculateFactors);
+			int second = nums[i] >> 10;
+			nums[2 * i] = first; // Number will right shift to 10 digits
+			nums[2 * i + 1] = second; // Nullify the sets bits which are set post 10 digits
+		}
+        
+        return nums;
+        }
 }
