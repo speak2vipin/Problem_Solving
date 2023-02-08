a536dcf2c2a75d8db824ccda7922f704f17b8096
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-02-08 22:47:30 +0530

    Time: 6 ms (45.66%), Space: 43 MB (40.86%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int jump(int[] nums) {
+       
+		
+		
+        PriorityQueue<Temp>pq = new PriorityQueue<Temp>(new Comparator<Temp>() {
+			@Override
+			public int compare(Temp o1, Temp o2) {
+				// TODO Auto-generated method stub
+				return o2.reach==o1.reach ? (o2.index-o1.index) : o2.reach-o1.reach;
+			}
+		});
+        int i = 0;
+        int jump = 1;
+        int N = nums.length;
+        if(N==1) {
+			return 0;
+		}
+        while(i+nums[i]<N-1) {
+            for(int j=i+1; j<=i+nums[i] && j<N-1; j++) {
+            	pq.offer(new Temp(j,j+nums[j]));
+            }
+            Temp top = pq.poll();
+            i = top.index;
+            jump++;
+            pq.clear();
+        }
+        return jump; 
+    
+    
+        
+    }
+}
+class Temp {
+    int index;
+    int reach;
+    public Temp(int index, int reach) {
+        this.index = index;
+        this.reach = reach;
+    }
+}
