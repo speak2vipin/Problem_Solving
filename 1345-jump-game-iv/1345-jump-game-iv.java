01e4c8cf653dacf3e4851f07e5768543ae9ed5af
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-03-05 17:37:35 +0530

    Time: 58 ms (91.26%), Space: 61.3 MB (36.67%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int minJumps(int[] arr) {
+        
+        Map<Integer, ArrayList<Integer>>valueToIndex = new HashMap<Integer, ArrayList<Integer>>();
+        int N = arr.length;
+        if(N<=1) {
+            return 0;
+        }
+       for(int i=0; i<N; i++) {
+			valueToIndex.computeIfAbsent(arr[i], val-> new ArrayList<Integer>()).add(i);
+        }
+        int steps = 0;
+        Queue<Integer> qe = new LinkedList<Integer> ();
+        qe.offer(0);
+        boolean[]visit = new boolean[N];
+        visit[0] = true;
+        while(!qe.isEmpty()) {
+            int size = qe.size();
+            for(int i=0; i<size; i++) {
+                int index = qe.poll();
+                if(index==N-1) {
+                    return steps;
+                }
+                if(index+1<N && !visit[index+1]) {
+                    qe.offer(index+1);
+                    visit[index+1] = true;
+                }
+                if(index-1>-1 && !visit[index-1]) {
+                    qe.offer(index-1);
+                    visit[index-1] = true;
+                }
+                for(int sameNumber : valueToIndex.getOrDefault(arr[index], new ArrayList<Integer>())) {
+                    if(!visit[sameNumber]){
+                        qe.offer(sameNumber);
+                        visit[sameNumber] = true;
+                    }
+                }
+                valueToIndex.remove(arr[index]);
+            }
+            steps++;
+        }
+       return -1; 
+    }
+}
