87eecf0579ac19092a62e95530fb0b9c485428af
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-03-07 13:50:30 +0530

    Time: 211 ms (9.62%), Space: 58.1 MB (61.77%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -1,25 +1,34 @@
 class Solution {
     public int maximumCandies(int[] candies, long k) {        
-        long high = 10_000_000;
-        long low = 0;
+       // long high = 10000000;
+        long low = 1;
+        long high = 0;
+        for(int candy : candies) {
+            high+=candy;
+        }
+        if(high<k) {
+            return 0;
+        }
 
         long mid = 0;
+        long ans = 0;
         
-        while(low<high) {
-            mid = (low + high+1)/2;
+        while(low<=high) {
+            mid = low + (high-low)/2;
             if(isPossible(candies, k, mid)) {
-                low = mid;
+                ans = mid;
+                low = mid+1;
             } else {
                 high = mid-1;
             }
         }
-        return (int)low;
+        return (int)ans;
     }
     
     boolean isPossible(int candies[], long k, long temp) {
         long count = 0;
         for(int candy : candies) {
-            count += ((long)candy/temp);
+            count += (long)candy/temp;
         }
         return count>=k;
     }
