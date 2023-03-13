ac9387b27c6157a3aedcbb342380d3e46c229624
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-03-13 15:12:22 +0530

    Time: 1 ms (19.60%), Space: 42.1 MB (13.74%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isTrue(root);
+    }
+    
+    boolean isTrue(TreeNode root) {
+        if(root==null) {
+            return true;
+        }
+        Queue<TreeNode>q = new LinkedList<TreeNode>();
+        q.offer(root);
+        while(!q.isEmpty()) {
+            int size = q.size();
+            ArrayList<Integer>l = new ArrayList<Integer>();
+            for(int i=0; i<size; i++) {
+                TreeNode top = q.poll();
+                if(top.val==101) {
+                    l.add(101);
+                } else {
+                    l.add(top.val);
+                    if(top.left!=null) {
+                        q.offer(top.left);
+                    } else {
+                        q.offer(new TreeNode(101));
+                    }
+                    if(top.right!=null) {
+                        q.offer(top.right);
+                    } else {
+                        q.offer(new TreeNode(101));
+                    }
+                }
+            }
+            if(!isPalindrom(l)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    boolean isPalindrom(ArrayList<Integer>l) {
+        int size = l.size();
+        if(size<=0) {
+            return true;
+        }
+        int st = 0;
+        int en = size-1;
+        while(st<en) {
+            if(l.get(st)!=l.get(en)) {
+                return false;
+            }
+            st++;
+            en--;
+        }
+        return true;
+    }
+}
