11cad2fc9460255be49fc972ebf742225ac9b830
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-03-18 22:53:02 +0530

    Time: 11 ms (100.00%), Space: 59.3 MB (100.00%) - LeetHub

--- 2592-maximize-greatness-of-an-array/2592-maximize-greatness-of-an-array.java
+++ 2592-maximize-greatness-of-an-array/2592-maximize-greatness-of-an-array.java
@@ -4,7 +4,6 @@ class Solution {
 		Arrays.sort(nums);
 		int N = nums.length;
 		int count = 0;
-		int j=0;
         for(int num : nums) {
             if(nums[count]<num) {
                 count++;
