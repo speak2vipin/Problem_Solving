bba73a0845c6c182325d6201ffec1028a9af9cd6
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-03-28 12:21:23 +0530

    Time: 23 ms (40.16%), Space: 42.3 MB (48.35%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -4,9 +4,7 @@ class Solution {
 		for (int i = 1; i <= amount; i++) {
 			int temp = Integer.MAX_VALUE;
 			for (int j = 0; j < coins.length; j++) {
-				if (i < coins[j]) {
-					continue;
-				} else {
+				if (i >= coins[j]) {
 					if(dp[Math.max(0, i - coins[j])] !=-1)
 					      temp = Math.min(temp, dp[Math.max(0, i - coins[j])] + 1);
 				}
