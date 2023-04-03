2b35779058b027425bc1d665b39448a58eedce60
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-04-03 12:49:03 +0530

    Time: 15 ms (98.39%), Space: 50.5 MB (69.91%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -2,37 +2,34 @@ class Solution {
     public int numRescueBoats(int[] people, int limit) {
         
         
-        int temp[] = new int[limit+1];
-        for(int weight : people) {
-            temp[weight] = temp[weight]+1;
-        }
-        int count = 0;
-        int tempWeight = 0;
-        int tempLimit = 0;
-        int index = limit;
-        while(index>0) {
-            tempLimit = limit;
-            tempWeight = index;
-            if(temp[tempWeight]<=0) {
-                index--;
-            } else {
-                count++;
-                int noOfPeople = 0;
-                while(tempWeight>0 && tempLimit > 0 && noOfPeople<2) {
-                    if(temp[tempWeight] > 0) {
-                        temp[tempWeight]--;
-                        tempLimit = tempLimit -tempWeight;
-                        tempWeight = tempLimit;
-                        noOfPeople++;
-                    } else {
-                        tempWeight--;
-                    }
-                }
-            }
-            
-        }
-        return count;
-    
-    
+		int temp[] = new int[limit + 1];
+		for (int weight : people) {
+			temp[weight] = temp[weight] + 1;
+		}
+		int count = 0;
+		int tempWeight = 0;
+		int tempLimit = 0;
+		int index = limit;
+		while (index > 0) {
+			tempLimit = limit;
+			tempWeight = index;
+			if (temp[tempWeight] <= 0) {
+				index--;
+			} else {
+				count++;
+				temp[tempWeight]--;
+				tempLimit = tempLimit - tempWeight;
+				tempWeight = tempLimit;
+				while (tempWeight > 0) {
+					if (temp[tempWeight] > 0) {
+						temp[tempWeight]--;
+						break;
+					} else {
+						tempWeight--;
+					}
+				}
+			}
+		}
+		return count;
     }
 }
