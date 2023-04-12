4e1bba973acfb2c74d52bed8f9fde67d7237af7d
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-04-12 12:39:11 +0530

    Time: 4 ms (85.61%), Space: 43.1 MB (11.63%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String simplifyPath(String path) {
+       
+        StringBuilder simplifyPath = new StringBuilder("");
+        
+        String[]splits = path.split("/");
+        int N = splits.length;
+        Stack<String> st = new Stack<>();
+        for(int i=0; i<N; i++) {
+        	if(!st.isEmpty() &&  splits[i].equals("..")) {
+        		st.pop();
+        	} else if(!splits[i].equals("") && 
+                      !splits[i].equals("..") && 
+                      !splits[i].equals(".")) {
+        		st.push(splits[i]);
+        	}
+        }
+        if(st.isEmpty()) {
+        	return "/";
+        }
+        
+        for(int i=0; i<st.size(); i++) {
+        	
+        	simplifyPath.append("/");
+        
+        	simplifyPath.append(st.get(i));
+        }
+        return simplifyPath.toString();
+    }
+}
