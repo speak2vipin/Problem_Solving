2bcd11cc4ba450a4d8d5a0b1f715af5918b55a94
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-04-22 01:14:08 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (8.34%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int findKthPositive(int[] arr, int k) {
+   /* public int findKthPositive(int[] arr, int k) {
         int temp = 1;
         int N = arr.length;
         for(int i=0; i<N; ) {
@@ -18,8 +18,24 @@ class Solution {
         while(k>0) {
             k--;
             temp++;
-        }
-        
-        return temp-1;
-    }
+        } 
+        return temp-1;        
+    } */
+    
+    // Logic : A[mid] - mid - 1 < K then number will lie in right hand side 
+    // or else left hand side
+    public int findKthPositive(int[] A, int k) {
+		int N = A.length;
+		int left = 0;
+		int right = N - 1;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (A[mid] - mid - 1 < k) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		return left+k;
+	}
 }
