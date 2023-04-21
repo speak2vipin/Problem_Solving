74fd8bee410e95777cf3886245bcc2d2d93c6fa0
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-04-21 19:04:19 +0530

    Time: 149 ms (17.12%), Space: 52.8 MB (21.92%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.java
+++ 0879-profitable-schemes/0879-profitable-schemes.java
@@ -20,23 +20,43 @@ class Solution {
                 Arrays.fill(dp[i][j], -1);
             }
         }
-        return noOfWays(0, 0, 0);
+        return noOfWaysTopToBottomApproach(0, 0, 0);
+        
+        // return noOfWaysBottomToUp(0, 0, 0);
     }
     
-    int noOfWays(int pos, int peopleCount, int currProfit) {
+    int noOfWaysTopToBottomApproach(int pos, int peopleCount, int currProfit) {
         if(pos >= len) {
             return currProfit >= minProfitNew ? 1 : 0;
         }
         if(dp[pos][peopleCount][currProfit] != -1) {
             return dp[pos][peopleCount][currProfit];
         }
-        int totalWays = noOfWays(pos+1, peopleCount, currProfit);
+        int totalWays = noOfWaysTopToBottomApproach(pos+1, peopleCount, currProfit);
         if((peopleCount + groupNew[pos]) <= maxPeople) {
-            totalWays += noOfWays(pos+1, Math.min(100, (peopleCount + groupNew[pos])), Math.min(100, (currProfit + profitNew[pos])));
+            totalWays += noOfWaysTopToBottomApproach(pos+1, Math.min(maxPeople, (peopleCount + groupNew[pos])), Math.min(100, (currProfit + profitNew[pos])));
         }
         
         dp[pos][peopleCount][currProfit] = totalWays%mod;
        
         return dp[pos][peopleCount][currProfit];
+    }
+    
+    int noOfWaysBottomToUp(int pos, int peopleCount, int currProfit) {
+        for(int count=0; count<=maxPeople; count++) {
+            dp[len][count][minProfitNew] = 1;
+        }
+        for(int index=len-1; index>-1; index--) {
+            for(int count=0; count<=maxPeople; count++) {
+                for(int profit=0; profit<=minProfitNew; profit++) {
+                    dp[index][count][profit] = dp[index+1][count][profit];
+                    if(count+groupNew[index]<=maxPeople) {
+                        dp[index][count][profit] = (dp[index][count][profit] 
+                                                    + dp[index+1][count+groupNew[index]][Math.min(minProfitNew, profit+profitNew[index])])%mod;
+                    }
+                }
+            }
+        }
+        return dp[0][0][0];
     }
 }
