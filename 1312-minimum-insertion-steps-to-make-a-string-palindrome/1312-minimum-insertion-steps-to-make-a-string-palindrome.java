6ccfd9ccf08485fa108d619a62fba6b55c9187f8
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-04-22 22:14:45 +0530

    Time: 63 ms (5.60%), Space: 44 MB (35.16%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,4 +1,13 @@
 class Solution {
+    // Logic :
+    // Typical question of Longest common sibsequence (Common letters in two String. Here it is not manadatory to have words consequitively)
+    // Find LCS in S and R (Reverse of S)
+    // Subtract N (Length of String) - LCS
+    // For calculating LCS
+        // Simply Count on number of letters in both S AND R
+        // If S.charAt(m-1) == S.charAt(n-1) => dp[m][n] = 1 + lcs(S,R,m-1, n-1)
+        // else dp[m][n] = Math.max(lcs(S, R, m-1, n), lcs(S, R, m, n-1))
+        // Return dp[m][n];
     int dp[][] = null;
     
     public int minInsertions(String s) {
