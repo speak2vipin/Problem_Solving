0c16436ba49208e2224aae917c09e28d4afbccfc
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-04-25 15:28:37 +0530

    Time: 10 ms (99.43%), Space: 43.2 MB (53.82%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.java
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.java
@@ -1,28 +1,29 @@
 class SmallestInfiniteSet {
     
-    int [] numbers;
+    HashSet<Integer> unique = null;
     int smallestPointer = 0;
-    PriorityQueue<Integer> pq = null;
+    PriorityQueue<Integer> minHeap = null;
     
     public SmallestInfiniteSet() {
-        pq = new PriorityQueue<Integer>();
-        numbers = new int[1001];
-        for(int i=1; i<1001; i++) {
-            numbers[i] = 1;
-            pq.offer(i);
-        }
+        unique = new HashSet<Integer>();
+        minHeap = new PriorityQueue<Integer>();
+        smallestPointer = 1;
     }
     
     public int popSmallest() {
-        int ans = pq.poll();
-        numbers[ans]--;
+        int ans = 0;
+        if(!minHeap.isEmpty()) {
+            ans = minHeap.poll();
+            unique.remove(ans);
+        } else {
+            ans = smallestPointer++;
+        }
         return ans;
     }
     
     public void addBack(int num) {
-        if(numbers[num]==0) {
-            numbers[num]++;
-            pq.offer(num);
+        if(num<smallestPointer && unique.add(num)) {
+            minHeap.offer(num);
         }
     }
 }
