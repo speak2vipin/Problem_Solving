43dba762ac24cd41fc8a7ea71ab9ab58cc452b51
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-04-25 13:15:38 +0530

    Time: 10 ms (99.43%), Space: 43.3 MB (41.36%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.java
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.java
@@ -1,26 +1,31 @@
 class SmallestInfiniteSet {
     
     int [] numbers;
+    int smallestPointer = 0;
     
     public SmallestInfiniteSet() {
         numbers = new int[1001];
         for(int i=1; i<1001; i++) {
             numbers[i] = 1;
         }
+        smallestPointer = 1;
     }
     
     public int popSmallest() {
-        int i = 1;
-        while(numbers[i]==0) {
-            i++;
+       // int i = 1;
+        while(numbers[smallestPointer]==0) {
+            smallestPointer++;
         }
-        numbers[i]--;
-        return i;
+        numbers[smallestPointer]--;
+        return smallestPointer++;
     }
     
     public void addBack(int num) {
         if(numbers[num]==0) {
             numbers[num] = 1;
+        }
+        if(num < smallestPointer) {
+            smallestPointer = num;
         }
     }
 }
