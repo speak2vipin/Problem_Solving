0d7f6fa2f78ea8a27fdf9cd92c3f796d40a358e4
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-04-26 07:32:05 +0530

    Time: 1 ms (100.00%), Space: 39.8 MB (78.69%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,8 +1,10 @@
 class Solution {
     public int addDigits(int num) {
         int temp = num;
+        // Evaluating is num a single digit number
         while((num/10)!=0) {
             temp = 0;
+            // Calculating sum of digits of num
             while(num!=0) {
                 temp += num%10; 
                 num = num/10;
