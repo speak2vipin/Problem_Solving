fd447fffd327d3ee45c4f612485aac4676c00838
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-04-27 09:16:12 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (31.56%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.java
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -1,11 +1,13 @@
 class Solution {
     public int bulbSwitch(int n) {
-        int x = 1;
+       /* int x = 1;
         int count = 0;
         while(x*x <= n) {
             count++;
             x++;
         }
         return count;
+        */
+        return (int)Math.sqrt(n);
     }
 }
