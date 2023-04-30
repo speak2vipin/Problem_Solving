1c2efdb29cc6329fc2481ebbf5d8d6ca04ed9eca
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-04-30 10:10:11 +0530

    Time: 36 ms (100.00%), Space: 66.2 MB (100.00%) - LeetHub

--- 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.java
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.java
@@ -1,4 +1,16 @@
 class Solution {
+    // Logic :  
+    // Store position of each number in matrix
+    // Iterate arr and 
+        // Check the count of each row and column
+        // If row==(columns size) 
+            // return index;
+        // else if col== (rows size)
+            // return index
+    
+    // TC- O(N*M + len) ~ O(N*M)
+    // SC- O(N*M)
+    
     public int firstCompleteIndex(int[] arr, int[][] mat) {
         Map<Integer, int[]> map = new HashMap<>();
         int N = mat.length;
@@ -10,10 +22,12 @@ class Solution {
         }
         
         int count = 0;
+        // WILL STORE NUMBER OF COLUMNS TO A ROW
         int row[] = new int[N];
+        // WILL STORE NUMBER OF ROWS TO A COLUMN
         int col[] = new int[M];
-
-        for(int i=0; i<arr.length; i++) {
+        int len = arr.length;
+        for(int i=0; i<len; i++) {
             int index[] = map.get(arr[i]);
             int x = index[0];
             int y = index[1];
@@ -26,9 +40,6 @@ class Solution {
                 return i;
             }
         }
-        return -1;
-        
-    }
-    
-        
+        return -1;   
+    }      
 }
