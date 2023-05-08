5802528d133fb15f5505506a5886dfb0cd67f6c7
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-05-08 14:08:37 +0530

    Time: 19 ms (25.37%), Space: 43.6 MB (37.31%) - LeetHub

--- 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.java
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.java
@@ -1,4 +1,8 @@
 class Solution {
+    // Logic
+        // Substring count can be
+            // Substring can start from 0 to i -> (i+1)
+            // Substring can end i to N-1 -> (N-1-i+1) = (N-i) 
     public long countVowels(String word) {
         HashSet<Character> vowels = new HashSet<>();
         vowels.add('a');vowels.add('e'); vowels.add('i'); vowels.add('o'); vowels.add('u');
