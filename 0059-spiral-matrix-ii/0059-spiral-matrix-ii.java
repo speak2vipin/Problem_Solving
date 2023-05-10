f1f4bb62f4c041c991e5b5c45f2db6442e0db907
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-05-10 10:59:04 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (17.46%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -11,33 +11,29 @@ class Solution {
                 ans[top][j] = count++;
             }
             top++;
-            if(top>n-1 || top>bottom) {
-                break;
-            }
+            if(top>n-1 || top>bottom) break;
+            
             
             for(int i=top; i<=bottom; i++) {
                 ans[i][right] = count++;
             }
             right--;
-            if(right<0 || left>right) {
-                break;
-            }
+            if(right<0 || left>right) break;
+            
             
             for(int j=right; j>=left; j--) {
                 ans[bottom][j] = count++;
             }
             bottom--;
-            if(bottom<0 || bottom<top) {
-                break;
-            }
+            if(bottom<0 || bottom<top) break;
+            
             
             for(int i=bottom; i>=top; i--) {
                 ans[i][left] = count++;
             }
             left++;
-            if(left>n-1 || left>right) {
-                break;
-            }
+            if(left>n-1 || left>right) break;
+            
         }
         return ans;
     }
