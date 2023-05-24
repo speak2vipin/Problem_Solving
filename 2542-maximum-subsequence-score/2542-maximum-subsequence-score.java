4cab825f2d8e5a5d4f68593600d47a31e08c35cc
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-05-24 11:01:03 +0530

    Time: 106 ms (32.76%), Space: 60 MB (50.00%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
@@ -1,24 +1,38 @@
 class Solution {
     public long maxScore(int[] nums1, int[] nums2, int k) {
         int N = nums1.length;
+        
+        // Create Pair array having nums1[i] and nums2[i]
         int pair[][] = new int[N][2];
         for(int i=0; i<N; i++) {
             pair[i] = new int[] {nums1[i], nums2[i]};
         }
+        
+        // Sort it in descending order by pair[1] i.e 1st index element
         Arrays.sort(pair, (a,b)->b[1]-a[1]);
+        
+        // Min heap to keep track of minimum element
+        // As Multiplication:- k elements in nums1 and nums2 i.e sum(nums[i]) * min (nums2[i])
+        // We have sorted pair by nums2[i] so min we can get from there
+        // To get minimum from nums1 we are using minHeap (So that we can make multiplcation maximum)
         PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>(k, (a,b)->a-b);
         long sum = 0;
+        
         for(int i=0; i<k; i++) {
             sum += pair[i][0];
             minHeap.offer(pair[i][0]);
         }
+        // Finding res = sum of nums1 * min u will get from pair[k-1][1]
         long res = sum * pair[k-1][1];
+        
+        // Iterate over array
         for(int i=k; i<N; i++) {
             sum = sum - minHeap.poll() + pair[i][0];
             minHeap.offer(pair[i][0]);
             long tempRes = sum * pair[i][1];
             res = Math.max(res, tempRes);    
         }
+        
         return res;
     }
 }
