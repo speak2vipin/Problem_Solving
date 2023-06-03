2721c181f758731e88ac794e09bef31152648a50
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-06-03 08:34:18 +0530

    Time: 90 ms (50.95%), Space: 69.5 MB (5.71%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -1,6 +1,9 @@
 class Solution {
+    // Logic:
+        //  Form adj matrix by given manager array
+    // Note if u want to traverse TREE as a GRAPH and want to calculate weight/ time/ val then keep time as DFS method parameter
     
-   Map<Integer, ArrayList<Integer>> adj = new HashMap<>();
+    Map<Integer, ArrayList<Integer>> adj = new HashMap<>();
     int res = Integer.MIN_VALUE;
 	public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
         
@@ -8,13 +11,15 @@ class Solution {
             if(manager[i]!=-1)
                 adj.computeIfAbsent(manager[i], val->new ArrayList<>()).add(i);
         }
- 
+        
         dfs(headID, informTime, 0);
         return res;  
     }
     
     int dfs(int node, int[] informTime, int time) {
+        // Check time at every step to find max time of a path
         res = Math.max(res, time);
+        
         ArrayList<Integer> subordinates = adj.getOrDefault(node,new ArrayList<Integer>());
         for(Integer subordinate : subordinates) {
             dfs(subordinate,informTime, time + informTime[node]);
