2307a6149fe457e353e09fee0eb3db0bd74ac86c
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-06-04 15:54:21 +0530

    Time: 8 ms (8.25%), Space: 44 MB (27.34%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,36 @@
+class Solution {
+    Map<Integer, ArrayList<Integer>> adj = new HashMap<>();
+    public int findCircleNum(int[][] isConnected) {
+        int N = isConnected.length;
+        for(int i=0; i<N; i++) {
+           // int temp[] = isConnected[i];
+            for(int j=0; j<N; j++) {
+                if(i==j) {
+                    continue;
+                } else if(isConnected[i][j]==1) {
+                    adj.computeIfAbsent(i, val->new ArrayList<Integer>()).add(j);
+                    adj.computeIfAbsent(j, val->new ArrayList<Integer>()).add(i);
+                }
+            }
+        }
+        int res = 0;
+        boolean visited[] = new boolean[N];
+        for(int i=0; i<N; i++) {
+            if(!visited[i]) {
+                res++;
+                dfs(i, visited);
+            }
+        
+        }
+        return res;
+    }
+    
+    void dfs(int source, boolean visited[]) {
+        visited[source] = true;
+        for(Integer node : adj.getOrDefault(source, new ArrayList<Integer>())) {
+            if(!visited[node]) {
+                dfs(node, visited);
+            }
+        }
+    }
+}
