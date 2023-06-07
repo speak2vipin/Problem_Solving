fafd3ef7a55d4275c33dfa5fa179594f9dcf30be
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-06-07 11:35:38 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (45.75%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -6,11 +6,14 @@ class Solution {
             int bitA = (a>>i)&1;
             int bitB = (b>>i)&1;
         	if(bitC==0) {
+                /*
         		if(bitA==1 && bitB==1) {
                     res +=2;
                 } else if((bitA^bitB)==1) {
                     res++;
-                }
+                }*/
+                
+                res += (bitA & 1) + (bitB & 1);
         	} else {
                 if(bitA==0 && bitB==0) {
                     res +=1;
