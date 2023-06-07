395701d7f4f0e0437c47414b2c482ee4345778f3
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-06-07 11:14:19 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (72.22%) - LeetHub

---
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minFlips(int a, int b, int c) { 
+        int res= 0;
+        for(int i=31; i>-1; i--) {
+            int bitC = (c>>i)&1;
+            int bitA = (a>>i)&1;
+            int bitB = (b>>i)&1;
+        	if(bitC==0) {
+        		if(bitA==1 && bitB==1) {
+                    res +=2;
+                } else if((bitA^bitB)==1) {
+                    res++;
+                }
+        	} else {
+                if(bitA==0 && bitB==0) {
+                    res +=1;
+                } 
+            }
+        }
+        return res;
+    }
+}
