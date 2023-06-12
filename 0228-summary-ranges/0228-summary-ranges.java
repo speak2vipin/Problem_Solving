57fbe06d695bf84dc9e617e92ec04b6dc876e735
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-06-12 12:17:17 +0530

    Time: 8 ms (8.18%), Space: 41.1 MB (25.23%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -2,33 +2,24 @@ class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> res = new ArrayList<>();
         int N = nums.length;
-        if(N==0) {
-            return res;
-        } 
         int st = 0;
         int en = 0;
-        String tempRes = "";
-        for(int i=1; i<N; i++) {
-            if(nums[en]+1 == nums[i]) {
-                en = i;
-            } else {
-                if(en==st) {
-                    tempRes = tempRes + nums[st];
-                } else {
-                    tempRes = nums[st] + "->" + nums[en];
-                }
-                res.add(tempRes);
-                tempRes = "";
-                st = i;
-                en = i;
+        
+        for(int i=0; i<N; i=en) {
+            String tempRes = "";
+            st = i;
+            en = i;
+            while(en+1<N && nums[en]+1==nums[en+1]) {
+                en++;
             }
+            if(st == en) {
+                tempRes += nums[st]; 
+            } else {
+                tempRes = nums[st] + "->" + nums[en];
+            }
+            en++;
+            res.add(tempRes);
         }
-        if(en==st) {
-            tempRes = tempRes + nums[st];
-        } else {
-            tempRes = nums[st] + "->" + nums[en];
-        }
-        res.add(tempRes);
         
         return res; 
     }
