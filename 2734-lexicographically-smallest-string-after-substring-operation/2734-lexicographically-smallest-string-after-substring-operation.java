375b51559cee16302651f2d0f4f38741a0b52240
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-06-12 12:49:12 +0530

    Time: 27 ms (66.67%), Space: 48.5 MB (16.67%) - LeetHub

--- 2734-lexicographically-smallest-string-after-substring-operation/2734-lexicographically-smallest-string-after-substring-operation.java
+++ 2734-lexicographically-smallest-string-after-substring-operation/2734-lexicographically-smallest-string-after-substring-operation.java
@@ -4,15 +4,18 @@ class Solution {
         char res[] = s.toCharArray();
         boolean flag = true;
         for(int i=0; i<N; i++) {
+            // Will cover all 'a' in the prefix
             if(s.charAt(i)=='a' && flag) {
                 continue;
-            } else if(s.charAt(i)=='a' && !flag) {
+            } else if(s.charAt(i)=='a' && !flag) { // No need to change further
                 break;
             } else {
+                // If we got char other than 'a'
                 res[i] = (char)(res[i]-1);
                 flag = false;
             }
         }
+        // If prefix have all a's
         if(flag) {
             res[N-1] = 'z';
         }
