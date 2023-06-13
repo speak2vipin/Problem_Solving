4df7b810c3a9042100c0d0cb9f9a6c7ec9e885e7
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-06-13 14:27:08 +0530

    Time: 51 ms (38.99%), Space: 50 MB (62.66%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -2,37 +2,37 @@ class Solution {
     public int equalPairs(int[][] grid) {
        
         Map<String, Integer> mapRow = new HashMap<>();
-        Map<String, Integer> mapCol = new HashMap<>();
+      //  Map<String, Integer> mapCol = new HashMap<>();
         int n = grid.length;
         
         for(int i=0; i<n; i++) {
             StringBuilder keyRow = new StringBuilder();
-            StringBuilder keyCol = new StringBuilder();
+           // StringBuilder keyCol = new StringBuilder();
             for(int j=0; j<n; j++) {
                 keyRow.append(grid[i][j]).append("_");
-                keyCol.append(grid[j][i]).append("_");
+                //keyCol.append(grid[j][i]).append("_");
             }
             mapRow.put(keyRow.toString(), mapRow.getOrDefault(keyRow.toString(), 0)+1);
             //mapCol.put(keyCol.toString(), mapCol.getOrDefault(keyCol.toString(), 0)+1);
         }
         
         int resRow = 0;
-        int resCol = 0;
-         for(int i=0; i<n; i++) {
-            StringBuilder keyRow = new StringBuilder();
+       // int resCol = 0;
+         for(int j=0; j<n; j++) {
+           // StringBuilder keyRow = new StringBuilder();
             StringBuilder keyCol = new StringBuilder();
-            for(int j=0; j<n; j++) {
-                keyRow.append(grid[i][j]).append("_");
-                keyCol.append(grid[j][i]).append("_");
+            for(int i=0; i<n; i++) {
+               // keyRow.append(grid[i][j]).append("_");
+                keyCol.append(grid[i][j]).append("_");
             }
             if(mapRow.get(keyCol.toString())!=null) {
                 resRow+= mapRow.get(keyCol.toString());
             }
-            if(mapCol.get(keyRow.toString())!=null) {
-            	resCol+= mapCol.get(keyRow.toString());
-            }
+           // if(mapCol.get(keyRow.toString())!=null) {
+            //	resCol+= mapCol.get(keyRow.toString());
+            //}
         }
-        return Math.max(resRow, resCol);
+        return resRow;
         
     
         
