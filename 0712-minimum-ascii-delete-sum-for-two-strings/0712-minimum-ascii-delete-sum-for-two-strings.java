2b7e9e155c447b0c34da7bb4993eb9e5e466e244
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-07-31 09:55:28 +0530

    Time: 20 ms (91.39%), Space: 43.9 MB (91.39%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,5 +1,14 @@
 class Solution {
+    // Logic is similar to longest common subsequence question
+    // create a 2-d array
+        // for all i's & j=0 -> sum of deleting all the character of s1
+        // for all j's & i=0 -> sum of deleting all the character of s2
+    // Simple iteration
+        // if(s1.charAt(i-1)==s2.charAt(j-1)) => dp[i][j] = dp[i-1][j-1];
+        // else dp[i][j] = Math.min(s1.charAt(i-1) + dp[i-1][j], s2.charAt(j-1) + dp[i][j-1])
+    
     public int minimumDeleteSum(String s1, String s2) {
+    
         int l1 = s1.length();
         int l2 = s2.length();
         
