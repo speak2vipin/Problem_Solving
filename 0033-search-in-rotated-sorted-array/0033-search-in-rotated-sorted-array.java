fba4906ed074bd7a10e1a653e0507fa7a754a853
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-08-10 14:09:53 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (70.94%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int st = 0;
+       /* int st = 0;
         int en = nums.length-1;
         while(st<=en) {
             int mid = en - (en-st)/2;
@@ -21,6 +21,31 @@ class Solution {
                 }
             }
         }
+        return -1;*/
+        
+ 
+        int st = 0;
+        int en = nums.length-1;
+        while(st<=en) {
+            int mid = en - (en-st)/2;
+            if(nums[mid]==target) {
+                return mid;
+            }
+            if(nums[st]<=nums[mid]) {
+                if(nums[st]<=target && target<nums[mid]) {
+                    en = mid-1;
+                } else {
+                    st = mid+1;
+                }
+            } else {
+                if(nums[en]>=target && target>nums[mid]) {
+                    st = mid+1;
+                } else {
+                    en = mid-1;
+                }
+            }
+        }
         return -1;
     }
+    
 }
