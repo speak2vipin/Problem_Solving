07310bd0e083e6c6f4945fd77d7447fe7f956acb
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-08-27 17:21:18 +0530

    Time: 139 ms (7.81%), Space: 54.6 MB (5.52%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -5,7 +5,7 @@ class Solution {
         // find pairs count which are increasing
     
     public int findLongestChain(int[][] pairs) {        
-	        Arrays.sort(pairs, (x,y) -> (x[1] - y[1]));
+	       /* Arrays.sort(pairs, (x,y) -> (x[1] - y[1]));
 	        int n = pairs.length;
 	        int j = 1;
 	        int prev = pairs[0][1];
@@ -18,5 +18,29 @@ class Solution {
 	            j++;
 	        }
 	        return count;  
+            */
+        Arrays.sort(pairs, (x,y)->(x[0]-y[0]));
+        int n = pairs.length;
+        int dp[][] = new int[n][n+1];
+        for(int []row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return helper(0, -1, pairs, dp);
     }
+    
+    int helper(int i, int prev, int pairs[][], int dp[][]) {
+		if (i >= pairs.length) {
+			return 0;
+		}
+		if (dp[i][prev+1] != -1) {
+			return dp[i][prev+1];
+		}
+		
+		int pick = 0;
+		if (prev==-1 || pairs[prev][1] < pairs[i][0]) {
+			pick = 1 + helper(i + 1, i, pairs, dp);
+		}
+		int no_Pick = helper(i+1, prev, pairs, dp);
+		return dp[i][prev+1] = Math.max(pick, no_Pick);
+	}
 }
