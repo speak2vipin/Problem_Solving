019f3128e5f6276db7ff5c3a67646881f7c69695
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-08-28 16:19:33 +0530

    Time: 0 ms (100.00%), Space: 40 MB (88.21%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -8,59 +8,44 @@ class MyStack {
     }
     
     public void push(int x) {
-        if(q1.isEmpty()) {
-            q2.offer(x);
-        } else {
-            q1.offer(x);
-        }
+        q1.offer(x);
     }
     
     public int pop() {
         int value = -1;
-        if(!empty()) {
-            if(q1.isEmpty()) {
-               int size = q2.size();
-               for(int i=0; i<size; i++) {
-                   value = q2.poll(); 
-                   if(i<size-1) {
-                      q1.offer(value);
-                   }
-               }
-            } else {
-               int size = q1.size();
-               for(int i=0; i<size; i++) {
-                   value = q1.poll(); 
-                   if(i<size-1) {
-                      q2.offer(value);
-                   }
-               }
+        
+        if(!q1.isEmpty()) {
+            int size = q1.size();
+            for(int i=0; i<size; i++) {
+                value = q1.poll(); 
+                if(i<size-1) {
+                    q2.offer(value);
+                }
             }
+            Queue<Integer> temp = q2;
+            q2 = q1;
+            q1 = temp;
         }
         return value;
     }
     
     public int top() {
         int value = -1;
-        if(!empty()) {
-            if(q1.isEmpty()) {
-               int size = q2.size();
-               for(int i=0; i<size; i++) {
-                   value = q2.poll(); 
-                   q1.offer(value);
-               }
-            } else {
-               int size = q1.size();
-               for(int i=0; i<size; i++) {
-                   value = q1.poll(); 
-                   q2.offer(value);
-               }
+        if(!q1.isEmpty()) {
+            int size = q1.size();
+            for(int i=0; i<size; i++) {
+                value = q1.poll(); 
+                q2.offer(value);
             }
-        }
+            Queue<Integer> temp = q2;
+            q2 = q1;
+            q1 = temp;
+        } 
         return value;
     }
     
     public boolean empty() {
-        return q1.isEmpty() && q2.isEmpty();
+        return q1.isEmpty();
     }
 }
 
