4d098d3d52327cd68e3f10886a7254ae68b74f99
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-09-05 14:42:11 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (93.28%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,7 +15,7 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        Map<Node, Node> map = new HashMap<>();
+       /* Map<Node, Node> map = new HashMap<>();
         Node res = null;
         Node curr = null;
         int index = 0;
@@ -42,5 +42,44 @@ class Solution {
             originalHead = originalHead.next;
         }
         return res;
+        */
+        
+        return copyRandomListWithoutSpaceComplexity(head);
     }
+    
+     public Node copyRandomListWithoutSpaceComplexity(Node head) {
+        if(head==null) {
+            return null;
+        }
+        Node originalHead = head;
+         
+        while(originalHead!=null) {
+            
+            Node temp = new Node(originalHead.val);
+        	Node prev = originalHead;
+            originalHead = originalHead.next;
+            prev.next = temp;
+            temp.next = originalHead;
+        }
+         
+        originalHead = head;
+        while(originalHead!=null) {
+            if(originalHead.random!=null) {
+                originalHead.next.random = originalHead.random.next;
+            }
+            originalHead = originalHead.next.next;
+        }
+         
+        originalHead = head;
+        Node res = originalHead.next;
+        while(originalHead!=null && originalHead.next!=null) {
+        	Node temp = originalHead.next;
+            originalHead.next = originalHead.next.next;
+            if(temp.next!=null) { 
+            	temp.next = temp.next.next;
+            }
+            originalHead = originalHead.next; 
+        }
+        return res;
+     }        
 }
