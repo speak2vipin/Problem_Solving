96ceb8bf02b7931725bf877337da64e8a5ed1286
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-09-09 18:21:06 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (73.34%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -3,12 +3,15 @@ class Solution {
     
     public int combinationSum4(int[] nums, int target) {
         Integer dp[] = new Integer[target + 1];
-		helper(dp, 0, target, nums);
-		return dp[0]==null?0:dp[0];
+		//helper(dp, 0, target, nums);
+		//return dp[0]==null?0:dp[0];
+        
+        helper(dp, target, target, nums);
+		return dp[target]==null?0:dp[target];
 	}
 
 	int helper(Integer dp[], int sum, int target, int nums[]) {
-        if(sum>target) {
+        /*if(sum>target) {
             return 0;
         }
         if(sum==target) {
@@ -22,5 +25,19 @@ class Solution {
             currentCombinations += helper(dp, sum+nums[i], target, nums);
         }
         return dp[sum]=currentCombinations;
+        */
+        if(sum<0) {
+            return 0;
+        } else if(sum==0) {
+            return 1;
+        }
+        if(dp[sum]!=null) {
+            return dp[sum];
+        }
+        int currentCombinations = 0;
+        for(int i=0; i<nums.length; i++) {
+            currentCombinations += helper(dp, sum-nums[i], target, nums);
+        }
+        return dp[sum] = currentCombinations;
     }
 }
