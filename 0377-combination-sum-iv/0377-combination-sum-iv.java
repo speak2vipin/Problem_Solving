68a6c228cd59b0602a227e3fb43e7b7868e8bc65
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-09-09 19:07:25 +0530

    Time: 1 ms (72.35%), Space: 39.5 MB (83.88%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,17 +1,14 @@
 class Solution {
-    Integer dp[] = null;
+    //Integer dp[] = null;
     
-    public int combinationSum4(int[] nums, int target) {
+    /*public int combinationSum4(int[] nums, int target) {
         Integer dp[] = new Integer[target + 1];
-		//helper(dp, 0, target, nums);
-		//return dp[0]==null?0:dp[0];
-        
-        helper(dp, target, target, nums);
-		return dp[target]==null?0:dp[target];
+		helper(dp, 0, target, nums);
+		return dp[0]==null?0:dp[0];
 	}
 
 	int helper(Integer dp[], int sum, int target, int nums[]) {
-        /*if(sum>target) {
+        if(sum>target) {
             return 0;
         }
         if(sum==target) {
@@ -25,7 +22,17 @@ class Solution {
             currentCombinations += helper(dp, sum+nums[i], target, nums);
         }
         return dp[sum]=currentCombinations;
-        */
+        
+    }
+    
+    public int combinationSum4(int[] nums, int target) {
+        Integer dp[] = new Integer[target + 1];
+        
+        helper(dp, target, target, nums);
+		return dp[target]==null?0:dp[target];
+	}
+    
+    int helper(Integer dp[], int sum, int target, int nums[]) {
         if(sum<0) {
             return 0;
         } else if(sum==0) {
@@ -39,5 +46,23 @@ class Solution {
             currentCombinations += helper(dp, sum-nums[i], target, nums);
         }
         return dp[sum] = currentCombinations;
+    }
+    */
+    
+     public int combinationSum4(int[] nums, int target) {
+        long dp[] = new long[target + 1];
+        helper(dp, target, nums);
+		return (int)dp[target];
+	}
+    
+    void helper(long dp[], int target, int nums[]) {
+        dp[0] = 1;
+        for(int currentSum=1; currentSum<=target; currentSum++) {
+            for(int i=0;i<nums.length; i++) {
+                if(currentSum-nums[i]>=0) {
+                    dp[currentSum] += dp[currentSum-nums[i]];
+                }
+            }
+        }
     }
 }
