a396153e3ada216afc8bcf61a40bc0a9f58ca08d
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-09-10 14:26:21 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (48.78%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -3,6 +3,17 @@ class Solution {
     // Remember this solution 
     // https://leetcode.com/problems/count-all-valid-pickup-and-delivery-options/discuss/4024311/Beats-100-oror-C%2B%2B-oror-Java-oror-Python-oror-DP-Recursive-and-Iterative-oror-Commented-Code
     
+    // Lets assume we already have n-1 pairs
+    // How many places would be required for nth pair
+        // For 1st item of nth pair
+        //  example n-1 --> 3 => total_items --> 6 => Total places for 4th pair
+        //  _X_X_X_X_X_X_ -> All elements+1 i.e. 2*4 - 1
+        // Total places for 1st element of 4th pair = 2*n - 1
+        // Total places for 2nd element of 4th pair = 2*n
+        // Total places = (2*n - 1) * (2*n)
+        // Total places if 1st order comes before 2nd = (2*n-1)*(2*n)/2 = (2*n-1)*n
+        // Same formula will be used for iterative calculations
+    
     long mod = 1000_000_007;
     
     public int countOrders(int n) {
