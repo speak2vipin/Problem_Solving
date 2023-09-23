8ea90db64ee2618b56c7ea62f7f575e2b58cbf07
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-09-23 10:32:22 +0530

    Time: 34 ms (81.34%), Space: 43.4 MB (61.75%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1048-longest-string-chain/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1048-longest-string-chain.java
@@ -0,0 +1,24 @@
+class Solution {
+    // Total TC : O(NlogN + N*l*l)
+    public int longestStrChain(String[] words) {
+        // TC : O(NlogN)
+        Arrays.sort(words, (l1, l2)-> l1.length()-l2.length());
+        int res = 0;
+        Map<String, Integer> memo = new HashMap<>();
+        // TC: N * l * l 
+        for(String word : words) {
+            memo.put(word, 1); 
+            // TC: length of word --> l*l
+            for(int i=0; i<word.length(); i++) {
+                StringBuilder sb = new StringBuilder(word);
+                // TC: length of word --> l
+                String next = sb.deleteCharAt(i).toString();
+                if(memo.keySet().contains(next)) {
+                    memo.put(word, Math.max(memo.get(word), memo.get(next)+1)); 
+                }
+            }
+            res = Math.max(res, memo.get(word));
+        }
+        return res;
+    }
+}
