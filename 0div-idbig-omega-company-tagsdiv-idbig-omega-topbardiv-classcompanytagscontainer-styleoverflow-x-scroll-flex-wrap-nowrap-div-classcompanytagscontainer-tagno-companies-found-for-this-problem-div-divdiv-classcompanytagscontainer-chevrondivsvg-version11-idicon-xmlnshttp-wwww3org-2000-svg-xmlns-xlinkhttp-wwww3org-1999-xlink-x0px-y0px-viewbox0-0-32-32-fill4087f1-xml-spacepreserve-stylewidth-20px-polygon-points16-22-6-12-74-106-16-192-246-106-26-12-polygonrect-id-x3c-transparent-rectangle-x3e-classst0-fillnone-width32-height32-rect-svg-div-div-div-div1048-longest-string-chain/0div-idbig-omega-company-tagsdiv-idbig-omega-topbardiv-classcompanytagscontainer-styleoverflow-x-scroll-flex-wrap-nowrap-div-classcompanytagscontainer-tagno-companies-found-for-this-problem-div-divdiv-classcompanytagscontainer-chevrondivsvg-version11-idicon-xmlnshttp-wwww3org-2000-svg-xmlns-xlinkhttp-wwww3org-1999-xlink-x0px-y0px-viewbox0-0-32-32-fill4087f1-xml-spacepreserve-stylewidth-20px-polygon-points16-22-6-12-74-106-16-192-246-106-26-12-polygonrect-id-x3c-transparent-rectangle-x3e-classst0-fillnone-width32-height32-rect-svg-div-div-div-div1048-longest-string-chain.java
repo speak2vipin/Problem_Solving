9ee14bac1b0acffbc4dbd923d1fd5e75b55c5a08
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-09-23 10:42:32 +0530

    Time: 37 ms (72.13%), Space: 43.4 MB (61.75%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1048-longest-string-chain/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1048-longest-string-chain.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1048-longest-string-chain/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1048-longest-string-chain.java
@@ -1,5 +1,6 @@
 class Solution {
     // Total TC : O(NlogN + N*l*l)
+    // Total SC : O(N) or in worst case values would be N*L i.e. New Intger object for every next i.e. substring at line number 19 
     public int longestStrChain(String[] words) {
         // TC : O(NlogN)
         Arrays.sort(words, (l1, l2)-> l1.length()-l2.length());
@@ -7,6 +8,9 @@ class Solution {
         Map<String, Integer> memo = new HashMap<>();
         // TC: N * l * l 
         for(String word : words) {
+            // At least one word must be inserted anywhere in word A to make it word B
+            // that's why we are not doing 
+            // memo.put(word, memo.getOrDefault(word, 0)+1);
             memo.put(word, 1); 
             // TC: length of word --> l*l
             for(int i=0; i<word.length(); i++) {
@@ -16,9 +20,8 @@ class Solution {
                 if(memo.keySet().contains(next)) {
                     memo.put(word, Math.max(memo.get(word), memo.get(next)+1)); 
                 }
-                res = Math.max(res, memo.get(word));
             }
-            
+            res = Math.max(res, memo.get(word));
         }
         return res;
     }
