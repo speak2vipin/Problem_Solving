cb21050ef48abe9fa0070f97c45ee09370a785e3
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-10-04 14:09:46 +0530

    Added solution - LeetHub

---
+++ Convert array into Zig-Zag fashion - GFG/convert-array-into-zig-zag-fashion.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static BufferedReader br;
+    static PrintWriter ot;
+    public static void main(String[] args) throws IOException{
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-- > 0){
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            String s[] = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                a[i] = Integer.parseInt(s[i]);
+            Solution soln = new Solution();
+            soln.zigZag(a, n);
+            boolean bool = isZigZag(a, n);
+            if(bool)
+                ot.println(1);
+            else
+                ot.println(0);
+        }
+        ot.close();
+    }
+    static boolean isZigZag(int a[], int n){
+        boolean flag = true;
+        for(int i = 1; i < n; i++){
+            if(flag){
+                if(a[i - 1] > a[i])
+                    return false;
+            } else{
+                if(a[i - 1] < a[i])
+                    return false;
+            }
+            flag = !flag;
+        }
+        return true;
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public void zigZag(int a[], int n){
+        // Code your solution here.
+        for(int i=1; i<n; i=i+2) {
+            if(a[i]<a[i-1]) {
+                swap(a, i, i-1);
+            }
+            if(i+1<n && a[i]<a[i+1]) {
+                swap(a, i, i+1);
+            }
+        }
+    }
+    void swap(int a[], int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
