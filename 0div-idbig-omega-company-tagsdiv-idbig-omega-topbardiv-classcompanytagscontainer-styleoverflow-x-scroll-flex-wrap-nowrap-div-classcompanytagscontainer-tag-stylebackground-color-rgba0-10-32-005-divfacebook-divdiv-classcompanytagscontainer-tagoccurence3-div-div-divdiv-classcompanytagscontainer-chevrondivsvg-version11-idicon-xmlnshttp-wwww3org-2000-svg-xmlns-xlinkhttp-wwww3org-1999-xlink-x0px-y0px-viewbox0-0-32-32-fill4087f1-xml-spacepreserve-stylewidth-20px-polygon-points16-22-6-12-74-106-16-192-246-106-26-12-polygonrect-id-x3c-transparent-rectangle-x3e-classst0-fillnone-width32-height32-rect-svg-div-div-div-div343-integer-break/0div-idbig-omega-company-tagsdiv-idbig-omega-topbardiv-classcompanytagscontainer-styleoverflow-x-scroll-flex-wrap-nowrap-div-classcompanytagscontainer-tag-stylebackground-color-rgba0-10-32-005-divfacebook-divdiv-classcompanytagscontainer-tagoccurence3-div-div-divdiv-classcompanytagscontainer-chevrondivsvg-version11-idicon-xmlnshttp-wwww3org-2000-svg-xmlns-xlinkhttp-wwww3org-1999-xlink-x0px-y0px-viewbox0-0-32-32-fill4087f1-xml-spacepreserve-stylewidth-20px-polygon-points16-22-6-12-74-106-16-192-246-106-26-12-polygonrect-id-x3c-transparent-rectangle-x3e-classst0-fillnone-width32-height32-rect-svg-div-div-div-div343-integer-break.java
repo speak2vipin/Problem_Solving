0eb994e90b1b7aea528850584ceee184c3dc5c87
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-10-06 11:52:33 +0530

    Time: 0 ms (100.00%), Space: 38.7 MB (97.29%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div343-integer-break/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div343-integer-break.java
@@ -0,0 +1,26 @@
+class Solution {
+    Integer dp[] = null;
+    public int integerBreak(int n) {
+        if(n<=3) {
+            return n-1;
+        }
+        dp = new Integer[n+1];
+        dp[2] = 2;
+        dp[3] = 3;
+        return helper(n);
+    }
+    
+    int helper(int n) {
+        if(n<=3) {
+            return n;
+        }
+        if(dp[n]!=null) {
+            return dp[n];
+        }
+        int ans = Integer.MIN_VALUE;
+        for(int i=1; i<n; i++) {
+            ans = Math.max(ans, i*helper(n-i));
+        }
+        return dp[n]=ans;
+    }
+}
