032674128fa3a827577b3f349aa4b96c0d599bbf
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-10-10 14:07:26 +0530

    Time: 65 ms (35.29%), Space: 57.5 MB (61.76%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2009-minimum-number-of-operations-to-make-array-continuous/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2009-minimum-number-of-operations-to-make-array-continuous.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2009-minimum-number-of-operations-to-make-array-continuous/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2009-minimum-number-of-operations-to-make-array-continuous.java
@@ -1,8 +1,9 @@
 class Solution {
     public int minOperations(int[] nums) {
-            int n = nums.length;
-            int ans = n;
             
+            int n = nums.length;
+            
+            // Find unique elements in Array
             HashSet<Integer> unique = new HashSet<>();
             for (int num : nums) {
                 unique.add(num);
@@ -10,30 +11,37 @@ class Solution {
             
             int[] newNums = new int[unique.size()];
             int index = 0;
-            
+            // Set unique elements to new  array
             for (int num : unique) {
                 newNums[index++] = num;
             }
-                
+            // Sort newly created Array    
             Arrays.sort(newNums);
-            //int N = newNums.length;
+            // Considering the left hand side elements as the starting point of every window
             int left = newNums[0];
+            // Right boundary 
             int right = left + n - 1;
+        
             int j = 0;
+            // Finding the length of maximum window 
+            // having elements fron Left to Right
+            
             while(j<newNums.length && newNums[j]<=right) {
                 j++;
             }
-            ans = j;
+        
+            int ans = j;
             
             for (int i = 1; i < newNums.length; i++) {
                 left = newNums[i];
                 right = left + n - 1;
+                // Recalculating the maximum window size
                 while(j<newNums.length && newNums[j]<=right) {
                 	j++;
                 }
                 ans = Math.max(ans, j-i);
             }
-            
+            // Rest of the elements
             return n-ans;
         }
     
