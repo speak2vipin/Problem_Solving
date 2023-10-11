0a422607b74b087cf9cfd2575d95339163a6a223
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-10-11 16:54:58 +0530

    Time: 57 ms (73.85%), Space: 75.4 MB (14.23%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2251-number-of-flowers-in-full-bloom/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2251-number-of-flowers-in-full-bloom.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2251-number-of-flowers-in-full-bloom/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2251-number-of-flowers-in-full-bloom.java
@@ -19,12 +19,12 @@ class Solution {
         
         int n = people.length;
         int m = flowers.length;
-        
+        // Copy original array to a new array so that we won't miss the order of people post sorting
         int originalOrderOfPeople[] = new int[n];
         System.arraycopy(people, 0, originalOrderOfPeople, 0, n);
-        
+        // Sort people by there arrival time in increasing order
         Arrays.sort(people);
-        
+        // Sort flowers by there starting time in increasing order
         Arrays.sort(flowers, (a, b)->a[0]-b[0]);
         
         Map<Integer, Integer> trackPeople = new HashMap<>();
@@ -33,18 +33,23 @@ class Solution {
         
         for(int i=0; i<n; i++) {
             int person = people[i];
+            // Add all the elements who's starting time is equal or lesser than arrival time of people[i]
             while(j<m && flowers[j][0]<=person) {
                 heap.offer(flowers[j][1]);
                 j++;
             }
+            // Remove all the elements who's end time is less than arrival time of people[i]
+            
             while(!heap.isEmpty() && heap.peek()<person) {
                 heap.poll();
             }
+            // place the heap size wrt arrival time
             trackPeople.put(person, heap.size());
         }
         
         int answer[] = new int[n];
         for(int i=0; i<n; i++) {
+            // Fetch value by arrival time ans place it to answer array
             answer[i] = trackPeople.get(originalOrderOfPeople[i]);
         }
         return answer;
