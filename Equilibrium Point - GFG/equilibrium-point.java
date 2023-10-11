375a822e7d41e3d58b41d2932003ad96ffbd754e
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-10-11 13:34:04 +0530

    Added solution - LeetHub

---
+++ Equilibrium Point - GFG/equilibrium-point.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            
+            //taking input n
+            int n = Integer.parseInt(br.readLine().trim());
+            long arr[] = new long[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //adding elements to the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = Long.parseLong(inputLine[i]);
+            }
+
+            Solution ob = new Solution();
+            
+            //calling equilibriumPoint() function
+            System.out.println(ob.equilibriumPoint(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    
+    // a: input array
+    // n: size of array
+    // Function to find equilibrium point in the array.
+    public static int equilibriumPoint(long arr[], int n) {
+
+        // Your code here
+        if(n==1) {
+            return 1;
+        }
+        long prefix[] = new long[n];
+        prefix[0] = arr[0];
+        
+        for(int i=1; i<n; i++) {
+            prefix[i] = prefix[i-1] + arr[i];
+        }
+        
+        for(int i=1; i<n-1; i++) {
+            if(prefix[i-1]==prefix[n-1]-prefix[i]) {
+                return i+1;
+            }
+        }
+        return -1;
+    }
+}
