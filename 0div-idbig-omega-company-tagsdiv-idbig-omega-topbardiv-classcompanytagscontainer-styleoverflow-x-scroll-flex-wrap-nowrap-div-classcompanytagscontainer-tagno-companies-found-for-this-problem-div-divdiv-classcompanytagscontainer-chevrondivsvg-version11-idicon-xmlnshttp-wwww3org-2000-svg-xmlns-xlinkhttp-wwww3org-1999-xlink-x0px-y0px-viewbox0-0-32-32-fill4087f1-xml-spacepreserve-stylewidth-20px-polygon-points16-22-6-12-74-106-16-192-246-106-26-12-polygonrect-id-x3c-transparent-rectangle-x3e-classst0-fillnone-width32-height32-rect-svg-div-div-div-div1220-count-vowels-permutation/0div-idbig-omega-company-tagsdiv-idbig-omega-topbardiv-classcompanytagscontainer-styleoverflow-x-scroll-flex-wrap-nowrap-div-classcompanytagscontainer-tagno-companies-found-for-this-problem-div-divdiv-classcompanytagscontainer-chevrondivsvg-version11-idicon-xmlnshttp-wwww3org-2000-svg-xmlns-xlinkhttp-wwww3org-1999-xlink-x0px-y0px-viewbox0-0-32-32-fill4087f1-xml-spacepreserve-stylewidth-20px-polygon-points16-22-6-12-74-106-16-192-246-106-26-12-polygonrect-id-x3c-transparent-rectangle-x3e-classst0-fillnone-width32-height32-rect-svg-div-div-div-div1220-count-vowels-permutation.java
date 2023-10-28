cb695efc567468f1a169e03e5cd504d0f750585e
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-10-28 22:57:27 +0530

    Time: 5 ms (99.01%), Space: 38.9 MB (94.39%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1220-count-vowels-permutation/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1220-count-vowels-permutation.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countVowelPermutation(int n) {
+        int mod = 1000000007;
+        long a = 1l;
+        long e = 1l;
+        long i = 1l;
+        long o = 1l;
+        long u = 1l;
+        
+        for(int k=1; k<n; k++) {
+            long a_next = e ;
+            long i_next = (a + e + o + u)%mod;
+            long e_next = (a + i)%mod;
+            
+            long o_next = (i + u)%mod;
+            long u_next = a%mod;
+            
+            a = a_next;
+            e = e_next;
+            i = i_next;
+            o = o_next;
+            u = u_next;
+        }
+        return (int)((a + e + i + o + u)%mod);
+    }
+}
