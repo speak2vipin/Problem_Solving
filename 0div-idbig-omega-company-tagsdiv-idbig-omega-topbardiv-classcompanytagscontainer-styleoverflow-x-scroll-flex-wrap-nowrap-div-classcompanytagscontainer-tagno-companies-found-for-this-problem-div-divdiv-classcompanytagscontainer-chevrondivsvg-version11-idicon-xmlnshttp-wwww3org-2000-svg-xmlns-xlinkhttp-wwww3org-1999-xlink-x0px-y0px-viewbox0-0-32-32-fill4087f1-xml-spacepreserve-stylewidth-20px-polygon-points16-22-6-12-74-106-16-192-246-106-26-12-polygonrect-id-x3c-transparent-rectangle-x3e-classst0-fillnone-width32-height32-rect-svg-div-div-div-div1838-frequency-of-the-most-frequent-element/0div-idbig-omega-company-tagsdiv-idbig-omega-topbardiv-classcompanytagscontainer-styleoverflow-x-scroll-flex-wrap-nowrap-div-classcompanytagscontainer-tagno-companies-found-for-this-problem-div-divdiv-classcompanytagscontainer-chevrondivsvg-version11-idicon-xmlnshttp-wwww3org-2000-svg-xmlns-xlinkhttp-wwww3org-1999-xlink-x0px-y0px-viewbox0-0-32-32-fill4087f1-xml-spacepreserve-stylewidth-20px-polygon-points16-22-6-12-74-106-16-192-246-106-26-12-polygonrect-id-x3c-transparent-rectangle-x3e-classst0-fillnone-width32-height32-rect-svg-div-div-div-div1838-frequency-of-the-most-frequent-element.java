6529b69b8a9cbb75d13087a82191a8f2a7bd988c
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-11-18 09:31:36 +0530

    Time: 30 ms (29.11%), Space: 57.9 MB (22.86%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1838-frequency-of-the-most-frequent-element/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1838-frequency-of-the-most-frequent-element.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1838-frequency-of-the-most-frequent-element/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1838-frequency-of-the-most-frequent-element.java
@@ -1,22 +1,28 @@
 class Solution {
-  
+    // Logic:
+        // Based on 2-pointer approach
+        // Simpley Sort the array
+        // calculate sum with every number array indexed number
+        // sum + k > num[j] (Max number) * number of elements
+            //continue;
+        // simple reduce elements from ith side untill sum + k > (max * number of elements)
+        // Calculate number of elements at every step
+        // return number count
     public int maxFrequency(int[] nums, int k) {
-        int n = nums.length;
         Arrays.sort(nums);
-       
-        int count = 1;
-        long sum = 0l;
-        int j = 0;
-        for(int i=0; i<n; i++) {
-            sum += nums[i];
-            while(sum+k < (1l*nums[i] * (i-j+1))) {
-                sum -= nums[j];
-                j++;
-            }
-            count = Math.max(count, i-j+1);
-        }
-        return count;
-            
+        int resCount = -1;
+        int n = nums.length;
+        long sum = 0;
+        int i = 0;
         
+        for(int j=0; j<n; j++) {
+            sum += nums[j];
+            while(sum + k < (1l * nums[j] * (j-i+1))) {
+                sum -= nums[i];
+                i++;
+            }
+            resCount = Math.max(resCount, j-i+1);
+        }
+        return resCount;
     }
 }
