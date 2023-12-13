6ca94acb2b4da58cda0c18a1c80c7558c7df1ac4
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-12-13 09:26:54 +0530

    Time: 3 ms (23.23%), Space: 44 MB (47.47%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1582-special-positions-in-a-binary-matrix/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1582-special-positions-in-a-binary-matrix.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int numSpecial(int[][] mat) {
+        
+        int m = mat.length;
+        int n = mat[0].length;
+        int ans = 0;
+        Set<Integer>rowset = new HashSet<>();
+        Set<Integer>colset = new HashSet<>();
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(mat[i][j]==1 && !rowset.contains(i) && !colset.contains(j)) {
+                    
+                    int row = 0, col = j;
+                    int oneCountRow = 0;
+                    int oneCountCol = 0;
+                    
+                    while(row<m) {
+                        if(mat[row][col]==1) {
+                            oneCountRow++;
+                        }
+                        row++;
+                    }
+                    
+                    row = i; col = 0;
+                    while(col<n) {
+                        if(mat[row][col]==1) {
+                            oneCountCol++;
+                        }
+                        col++;
+                    }
+                    
+                    if(oneCountRow==1 && oneCountCol==1) {
+                        ans++;
+                    }
+                    rowset.add(i);
+                    colset.add(j);
+                } 
+            }
+        }
+        return ans;
+    
+    }
+}
