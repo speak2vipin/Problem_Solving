2a2b4d759cfe3fd387e017b22c051c34f4997d9f
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2023-12-17 23:14:54 +0530

    Time: 163 ms (91.53%), Space: 83.2 MB (5.08%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2353-design-a-food-rating-system/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2353-design-a-food-rating-system.java
@@ -0,0 +1,46 @@
+class FoodRatings {
+    Map<String, PriorityQueue<Object[]>> cuisinesFoodMap = null;
+	Map<String, String> foodsCuisinesMap = null;
+	Map<String, Integer> foodsRatingsMap = null;
+
+	public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+		cuisinesFoodMap = new HashMap<>();
+		foodsCuisinesMap = new HashMap<>();
+		foodsRatingsMap = new HashMap<>();
+
+		int n = foods.length;
+		for (int i = 0; i < n; i++) {
+			foodsCuisinesMap.put(foods[i], cuisines[i]);
+			foodsRatingsMap.put(foods[i], ratings[i]);
+			cuisinesFoodMap
+					.computeIfAbsent(cuisines[i],
+							val -> new PriorityQueue<Object[]>((x, y) -> ((Integer) x[0]).compareTo((Integer) y[0]) == 0
+									? (((String) x[1]).compareTo((String) y[1]))
+									: ((Integer) y[0]).compareTo((Integer) x[0])))
+
+					.offer(new Object[] { ratings[i], foods[i] });
+		}
+	}
+
+	public void changeRating(String food, int newRating) {
+		String cuisine = foodsCuisinesMap.get(food);
+		cuisinesFoodMap.get(cuisine).offer(new Object[] { newRating, food });
+		foodsRatingsMap.put(food, newRating);
+	}
+
+	public String highestRated(String cuisine) {
+		Object[] top = cuisinesFoodMap.get(cuisine).peek();
+		while (foodsRatingsMap.get(top[1]).compareTo((Integer)top[0])!=0) {
+			cuisinesFoodMap.get(cuisine).poll();
+			top = cuisinesFoodMap.get(cuisine).peek();
+		}
+		return (String) top[1];
+	}
+}
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings obj = new FoodRatings(foods, cuisines, ratings);
+ * obj.changeRating(food,newRating);
+ * String param_2 = obj.highestRated(cuisine);
+ */
