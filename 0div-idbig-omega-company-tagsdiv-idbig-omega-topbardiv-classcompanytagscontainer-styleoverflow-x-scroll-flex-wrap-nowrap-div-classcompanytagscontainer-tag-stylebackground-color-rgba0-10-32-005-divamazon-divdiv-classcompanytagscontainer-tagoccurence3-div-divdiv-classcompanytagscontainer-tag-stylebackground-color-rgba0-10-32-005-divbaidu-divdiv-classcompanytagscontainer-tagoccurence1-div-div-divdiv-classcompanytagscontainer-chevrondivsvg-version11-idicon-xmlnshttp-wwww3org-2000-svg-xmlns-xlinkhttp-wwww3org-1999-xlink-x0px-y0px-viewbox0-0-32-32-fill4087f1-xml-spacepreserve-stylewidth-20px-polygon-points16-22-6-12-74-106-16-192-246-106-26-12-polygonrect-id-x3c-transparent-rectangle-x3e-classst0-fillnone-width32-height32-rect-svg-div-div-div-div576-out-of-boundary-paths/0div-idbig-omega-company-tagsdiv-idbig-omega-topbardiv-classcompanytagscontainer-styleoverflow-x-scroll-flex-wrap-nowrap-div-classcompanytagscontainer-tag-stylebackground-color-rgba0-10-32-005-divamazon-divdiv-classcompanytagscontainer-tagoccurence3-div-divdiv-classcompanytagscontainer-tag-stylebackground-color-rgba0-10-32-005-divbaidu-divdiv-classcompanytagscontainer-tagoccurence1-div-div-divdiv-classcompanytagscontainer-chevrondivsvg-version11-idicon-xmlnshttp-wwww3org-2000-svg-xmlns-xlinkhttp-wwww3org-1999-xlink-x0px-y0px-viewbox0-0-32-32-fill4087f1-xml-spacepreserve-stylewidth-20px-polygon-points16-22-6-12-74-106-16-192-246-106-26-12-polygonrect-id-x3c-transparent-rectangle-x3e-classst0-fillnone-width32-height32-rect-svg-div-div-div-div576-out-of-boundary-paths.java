9a8d5a2141afdee417faf69b02ea2bd89461dee5
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2024-01-27 00:46:29 +0530

    Time: 11 ms (24.80%), Space: 41.9 MB (74.40%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbaidu-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div576-out-of-boundary-paths/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbaidu-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div576-out-of-boundary-paths.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbaidu-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div576-out-of-boundary-paths/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbaidu-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div576-out-of-boundary-paths.java
@@ -25,16 +25,17 @@ class Solution {
                     
                     
                     if (i == m - 1) count = (count + dp[i][j]) % mod;
-          if (j == n - 1) count = (count + dp[i][j]) % mod;
-          if (i == 0) count = (count + dp[i][j]) % mod;
-          if (j == 0) count = (count + dp[i][j]) % mod;
-          temp[i][j] = 
-                (((i>0 ? dp[i-1][j]:0) + (i+1<m ? dp[i+1][j] : 0))%mod
-                + ((j>0? dp[i][j-1]:0) + (j+1<n ? dp[i][j+1] : 0))%mod)%mod;
-          //temp[i][j] = (
-            //  ((i > 0 ? dp[i - 1][j] : 0) + (i+1 < m  ? dp[i + 1][j] : 0)) % mod +
-              //((j > 0 ? dp[i][j - 1] : 0) + (j+1 < n ? dp[i][j + 1] : 0)) % mod
-          //) % mod;
+                    if (j == n - 1) count = (count + dp[i][j]) % mod;
+                    if (i == 0) count = (count + dp[i][j]) % mod;
+                    if (j == 0) count = (count + dp[i][j]) % mod;
+                    temp[i][j] = 
+                        ((
+                            (i>0 ? dp[i-1][j]:0) + 
+                            (i+1<m ? dp[i+1][j] : 0))%mod
+                    + (
+                        (j>0? dp[i][j-1]:0) + 
+                        (j+1<n ? dp[i][j+1] : 0))%mod)%mod;
+          
                 }
             }
            dp = temp;
