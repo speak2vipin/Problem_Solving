a0a5f7a61115b89425a72bd6454b368baf1a0d11
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2024-02-01 21:21:58 +0530

    Time: 25 ms (38.13%), Space: 62.1 MB (5.21%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2966-divide-array-into-arrays-with-max-difference/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2966-divide-array-into-arrays-with-max-difference.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2966-divide-array-into-arrays-with-max-difference/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2966-divide-array-into-arrays-with-max-difference.java
@@ -1,28 +1,22 @@
 class Solution {
-    public int[][] divideArrayMy(int[] nums, int k) {
+    public int[][] divideArray(int[] nums, int k) {
         Arrays.sort(nums);
         int n = nums.length;
         int N = n/3;
         int ans[][] = new int[N][3];
-        int temp[] = null;
         int index = 0;
-        int min = -1;
-        for(int i=0; i<n; i++) {
-            if((i%3)==0) {
-                temp = new int[3];
-                ans[index++] = temp;
-                min = nums[i];
-            } else {
-                if(nums[i] - min > k) {
-                    return new int[][]{};
-                }
+        int st = 0;
+        while(st<n) {
+            if(nums[st+2]-nums[st]>k) {
+                return new int[][]{};
             }
-            temp[i%3] = nums[i];
+            ans[index++] = new int[] {nums[st], nums[st+1], nums[st+2]};
+            st += 3;
         }
         return ans;
     }
     
-    public int[][] divideArray(int[] nums, int k) {
+    public int[][] divideArrayPrev(int[] nums, int k) {
         Arrays.sort(nums);
         int n = nums.length;
         int ans[][] = new int[n/3][];
