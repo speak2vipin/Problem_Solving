50a42a0dfb6a6ef67f2b426abd3210612a6fb941
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2024-02-18 23:33:22 +0530

    Time: 71 ms (78.13%), Space: 101.7 MB (8.25%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2402-meeting-rooms-iii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2402-meeting-rooms-iii.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int mostBooked(int n, int[][] meetings) {
+         PriorityQueue<long[]> used = 
+             new PriorityQueue<long[]>((a, b) -> a[1] != b[1] ? Long.compare(a[1], b[1]) : Long.compare(a[2], b[2]));
+        //PriorityQueue<long[]> used = new PriorityQueue<long[]>(
+          //(x,y)->x[1]==y[1] ? (int)(x[0]-y[0]) : (int)(x[1]-y[1]));
+      PriorityQueue<Integer> unused = new PriorityQueue<Integer>();
+      
+      //Arrays.sort(meetings, (x,y)->x[0]==y[0] ? x[1]-y[1] : x[0]-y[0]);
+Arrays.sort(meetings, (a, b) -> a[0] != b[0] ? Integer.compare(a[0], b[0]) : Integer.compare(a[1], b[1]));
+      
+        for(int i=0; i<n; i++) {
+          unused.offer(i);
+      }
+      
+      int meetingsCount[] = new int[n];
+      
+      for(int i=0; i<meetings.length; i++) {
+          int st = meetings[i][0];
+          int en = meetings[i][1];
+          
+          while(!used.isEmpty() && used.peek()[1]<=st) {
+              unused.offer((int)used.poll()[2]);
+          }
+          
+          if(!unused.isEmpty()) {
+              int available = unused.poll();
+              meetingsCount[available]++;
+              used.offer(new long[]{st, en, available});
+          } else {
+              long top[] = used.poll();
+              meetingsCount[(int)top[2]]++;
+              used.offer(new long[]{top[1], top[1] + (en-st),  top[2]});
+          }   
+      }
+      
+      int temp = Integer.MIN_VALUE;
+      int ans = Integer.MAX_VALUE;
+      for(int i=0; i<n; i++) {
+          if(temp<meetingsCount[i]) {
+              ans = i;
+              temp = meetingsCount[i];
+          }
+      }
+      return ans;   
+        
+    }
+}
