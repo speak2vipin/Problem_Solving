c16f17aa518bec31af504548ed0e431c6585f0ac
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2024-02-25 20:11:09 +0530

    Time: 85 ms (47.00%), Space: 108.3 MB (45.00%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2092-find-all-people-with-secret/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2092-find-all-people-with-secret.java
@@ -0,0 +1,42 @@
+class Solution {
+    public static List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
+		
+		Map<Integer, ArrayList<int[]>> adj = new HashMap<>();
+		for (int meet[] : meetings) {
+			adj.computeIfAbsent(meet[0], val -> new ArrayList<int[]>()).add(new int[] { meet[1], meet[2] });
+			adj.computeIfAbsent(meet[1], val -> new ArrayList<int[]>()).add(new int[] { meet[0], meet[2] });
+		}
+		PriorityQueue<int[]> pq = new PriorityQueue<int[]>((x, y) -> (x[1] - y[1]));
+		Set<Integer> res = new HashSet<>();
+		
+		pq.offer(new int[] {firstPerson, 0});
+        pq.offer(new int[]{0, 0});
+        boolean vst[] = new boolean[n];
+		while (!pq.isEmpty()) {
+			int top[] = pq.poll();
+            int time = top[1];
+            int person = top[0];
+            if(res.contains(person)) {
+                continue;
+            }
+            res.add(person);
+            vst[person] = true;
+			if (adj.get(person) != null) {
+				for (int[] x : adj.get(person)) {
+					if (x[1] >= time && !vst[x[0]]) {
+						pq.offer(new int[] { x[0], x[1] });
+					}
+				}
+			}
+		}
+                                             
+         //for(int i = 0; i<n; i++) {
+           //  if(vst[i]) {
+             //    res.add(i);
+             //}
+         //}                                    
+		return new ArrayList<>(res);
+
+	}
+                    
+}
