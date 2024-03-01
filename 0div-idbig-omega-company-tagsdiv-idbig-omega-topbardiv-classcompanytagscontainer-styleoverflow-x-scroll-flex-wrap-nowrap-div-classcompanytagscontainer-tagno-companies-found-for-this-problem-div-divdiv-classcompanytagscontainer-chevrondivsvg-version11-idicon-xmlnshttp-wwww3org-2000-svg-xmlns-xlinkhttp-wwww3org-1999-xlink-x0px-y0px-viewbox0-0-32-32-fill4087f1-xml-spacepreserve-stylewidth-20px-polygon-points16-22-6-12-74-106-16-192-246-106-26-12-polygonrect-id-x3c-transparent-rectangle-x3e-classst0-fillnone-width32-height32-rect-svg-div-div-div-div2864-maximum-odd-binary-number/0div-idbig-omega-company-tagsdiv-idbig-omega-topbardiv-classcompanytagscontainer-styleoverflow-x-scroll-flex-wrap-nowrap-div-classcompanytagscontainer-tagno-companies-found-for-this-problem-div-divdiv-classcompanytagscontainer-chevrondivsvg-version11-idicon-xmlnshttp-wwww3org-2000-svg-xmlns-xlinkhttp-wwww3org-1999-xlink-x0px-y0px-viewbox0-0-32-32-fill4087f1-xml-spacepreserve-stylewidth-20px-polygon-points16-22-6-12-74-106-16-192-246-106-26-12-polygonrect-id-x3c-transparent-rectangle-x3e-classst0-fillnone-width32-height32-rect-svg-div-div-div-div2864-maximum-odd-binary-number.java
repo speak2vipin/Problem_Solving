48a730fc521502a3725adcaec0c89231d6b59054
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2024-03-01 13:17:46 +0530

    Time: 1 ms (100.00%), Space: 42.8 MB (96.48%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2864-maximum-odd-binary-number/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2864-maximum-odd-binary-number.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2864-maximum-odd-binary-number/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2864-maximum-odd-binary-number.java
@@ -5,10 +5,11 @@ class Solution {
             return s;
         }
         
-        int i = 0;
-        int j = 0;
+        //int i = 0;
+        //int j = 0;
         char charArray[] = s.toCharArray();
-        while(i<n) {
+        
+        /*while(i<n) {
             if(s.charAt(i)=='1') {
                 charArray[j] = s.charAt(i);
                 if(i!=j) {
@@ -18,10 +19,27 @@ class Solution {
             } 
             i++;
         }
+        
         if(j!=n) {
             charArray[n-1] = charArray[j-1];
             charArray[j-1] = '0';
+        }*/
+        int i = 0;
+        int j = n-1;
+        while(i<=j) {
+            if(charArray[i]=='1') {
+                i++;
+            }
+            if(charArray[j]=='0') {
+                j--;
+            }
+            if(i<j && charArray[i] == '0' && charArray[j] == '1') {
+                charArray[i] = '1';
+                charArray[j] = '0';
+            }
         }
+        charArray[i-1] = '0';
+        charArray[n-1] = '1';
         return new String(charArray);
     }
 }
