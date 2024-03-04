5a191b04ac92122f8cca5de17a36c1864cada72b
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2024-03-04 17:33:35 +0530

    Time: 2 ms (97.13%), Space: 43.1 MB (9.87%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div948-bag-of-tokens/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div948-bag-of-tokens.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int index = 0;
+        int count = 0;
+        int i = 0;
+        int j = tokens.length-1;
+        int total = 0;
+        while(i<tokens.length) {
+            total += tokens[i++];
+        }
+        if(total<=power) {
+            return tokens.length;
+        }
+         i = 0;
+        while(i<=j) {
+            if(power<tokens[i]) {
+                if(i==j || count==0) {
+                    break;
+                }
+                count--;
+                power += tokens[j];
+                j--;
+            } else {
+                power -= tokens[i];
+                count++;
+                i++;
+            }
+        }
+        return count;
+        
+    }
+}
