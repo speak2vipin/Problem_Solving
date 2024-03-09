973e7ce9ccc2a5c8e1fdfbb98dbf4a5ee4d9b8f3
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2024-03-09 09:51:58 +0530

    Time: 5 ms (33.74%), Space: 58.2 MB (12.88%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2105-watering-plants-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2105-watering-plants-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minimumRefill(int[] plants, int capacityA, int capacityB) {
+        int i = 0;
+        int j = plants.length-1;
+        int result = 0;
+        int tempCapacityA = capacityA;
+        int tempCapacityB = capacityB;
+        
+        while(i<=j) {
+            if(i==j) {
+                if(plants[i]>Math.max(tempCapacityA, tempCapacityB)) {
+                   result++; 
+                }
+            } else {
+                if(plants[i]<=tempCapacityA) {
+                    tempCapacityA -= plants[i];
+                } else {
+                    result++;
+                    tempCapacityA = capacityA;
+                    tempCapacityA -= plants[i];
+
+                }
+                if(plants[j]<=tempCapacityB) {
+                    tempCapacityB -= plants[j];
+                } else {
+                    result++;
+                    tempCapacityB = capacityB;
+                    tempCapacityB -= plants[j];
+                }
+            }
+            i++;
+            j--;
+        }
+        return result;
+    }
+}
