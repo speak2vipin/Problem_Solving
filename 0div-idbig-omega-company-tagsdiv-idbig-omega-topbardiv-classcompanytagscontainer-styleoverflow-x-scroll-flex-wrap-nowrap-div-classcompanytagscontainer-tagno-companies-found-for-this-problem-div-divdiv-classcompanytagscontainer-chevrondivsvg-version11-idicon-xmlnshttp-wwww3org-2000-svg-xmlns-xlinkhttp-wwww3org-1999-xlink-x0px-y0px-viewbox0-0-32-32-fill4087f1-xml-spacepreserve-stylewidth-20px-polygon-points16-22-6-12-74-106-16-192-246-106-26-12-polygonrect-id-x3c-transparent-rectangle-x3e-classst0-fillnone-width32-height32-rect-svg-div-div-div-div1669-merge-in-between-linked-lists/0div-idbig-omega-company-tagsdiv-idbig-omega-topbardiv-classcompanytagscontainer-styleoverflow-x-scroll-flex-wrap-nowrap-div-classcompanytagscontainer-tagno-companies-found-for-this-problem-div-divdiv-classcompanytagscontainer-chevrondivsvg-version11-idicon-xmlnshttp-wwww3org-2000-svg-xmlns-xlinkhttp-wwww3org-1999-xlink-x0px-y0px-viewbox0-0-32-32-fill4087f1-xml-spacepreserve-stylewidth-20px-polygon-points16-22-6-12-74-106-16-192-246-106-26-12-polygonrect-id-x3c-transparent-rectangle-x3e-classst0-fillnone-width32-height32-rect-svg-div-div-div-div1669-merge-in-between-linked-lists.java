97f83c87953b218b63cadbfe6b482ee51094f867
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2024-03-20 18:05:18 +0530

    Time: 8 ms (9.35%), Space: 49.3 MB (5.98%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1669-merge-in-between-linked-lists/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1669-merge-in-between-linked-lists.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        /*ListNode head = list1;
+       
+        int count = 1;    
+        while(count<a) {
+            head = list1.next;
+            count++;
+        }
+        ListNode end = head;
+        while(count<b+1) {
+            end = end.next;
+            count++;
+        }
+        head.next = list2;
+        ListNode end2 = list2;
+        while(end2.next!=null) {
+            end2 = end2.next;
+        }
+        end2.next = end.next;
+        return list1;*/
+        HashMap<Integer, ListNode> map = new HashMap<>();
+        ListNode head = list1;
+        int count = 0;
+        while(head!=null) {
+            map.put(count++, head);
+            head = head.next;
+        }
+        map.get(a-1).next = list2;
+        ListNode end = list2;
+        while(end.next!=null) {
+            end = end.next;
+        }
+        end.next = map.get(b).next;
+        return list1;
+        
+    }
+}
