5dfab5730dc8cd194c5eb48b5d6799f485a27595
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2024-05-01 23:17:54 +0530

    Time: 0 ms (100.00%), Space: 42 MB (15.02%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -3,7 +3,7 @@ class Solution {
         int index = word.indexOf(ch);
         StringBuilder sb = 
             new StringBuilder(word.substring(0, index+1)).reverse().append(word.substring(index+1));
-        return index==-1? word:sb.toString();
+        return sb.toString();
         
     }
 }
