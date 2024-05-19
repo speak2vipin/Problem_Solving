a9adc39fefd9bf6ed662c922c9544c509bd80be4
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2024-05-19 22:31:37 +0530

    Time: 1 ms (100.00%), Space: 43.2 MB (100.00%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        int n = nums.length;
+        for(int i=0; i<n-1; i++) {
+            if(nums[i]%2==0 && nums[i+1]%2==0) {
+                return false;
+            } else if(nums[i]%2!=0 && nums[i+1]%2!=0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
