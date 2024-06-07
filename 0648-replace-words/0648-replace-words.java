5306ddb5df5eba815196e46d5cc539f011029c36
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2024-06-08 00:32:29 +0530

    Time: 52 ms (31.90%), Space: 56 MB (25.10%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.java
@@ -0,0 +1,62 @@
+class Solution {
+    Trie root = null;
+    public String replaceWords(List<String> dictionary, String sentence) {
+        for(String d : dictionary) {
+            formTrie(d);
+        }
+        String sen[] = sentence.split(" ");
+        for(int i=0; i<sen.length; i++) {
+            sen[i] = traverse(sen[i]);
+        }
+        String res = "";
+        for(String s : sen) {
+            res += s + " ";
+        }
+        return res.trim();
+        
+    }
+    
+    String traverse(String s) {
+        Trie temp = root;
+        String rel = "";
+        for(int i=0; i<s.length(); i++) {
+            int index = s.charAt(i)-'a';
+            if(temp.trie[index]==null) {
+                return s;
+            } else {
+                rel += s.charAt(i);
+                temp = temp.trie[index];
+                if(temp.isDone) {
+                    return rel;
+                }
+            }
+        }
+       return s;
+    }
+    
+    void formTrie(String s) {
+        Trie temp = null;
+        if(root == null) {
+            root = new Trie();
+        }
+        temp = root;
+        for(int i=0; i<s.length(); i++) {
+            int index = s.charAt(i)-'a';
+            if(temp.trie[index]==null) {
+                temp.trie[index] = new Trie();
+            }
+            temp = temp.trie[index];
+        }
+        temp.isDone = true;        
+    }
+}
+
+
+class Trie {
+    Trie trie[];
+    boolean isDone;
+    public Trie() {
+        trie = new Trie[26];
+        isDone = false;
+    }
+}
