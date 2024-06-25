00eb1502555428421f4f65ee22dd3130e005254c
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2024-06-25 18:49:14 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (73.87%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
@@ -14,9 +14,14 @@
  * }
  */
 class Solution {
-    public TreeNode bstToGst(TreeNode root) {
+    public TreeNode bstToGstWithRecurssion(TreeNode root) {
         int nodeSum[] = new int[1];
         helper(root, nodeSum);
+        return root;
+    }
+    
+    public TreeNode bstToGst(TreeNode root) {
+        helperWithoutRecurssion(root);
         return root;
     }
   
@@ -28,5 +33,21 @@ class Solution {
         nodeSum[0]+= node.val;
         node.val = nodeSum[0];
         helper(node.left, nodeSum);
+    }
+    
+    void helperWithoutRecurssion(TreeNode node) {
+        Stack<TreeNode> st = new Stack<>();
+        int nodeSum = 0;
+        while(!st.isEmpty() || node!=null) {
+            if(node!=null) {
+                st.push(node);
+                node = node.right;
+                continue;
+            }
+            node = st.pop();
+            nodeSum += node.val;
+            node.val = nodeSum;
+            node = node.left;
+        }
     }
 }
