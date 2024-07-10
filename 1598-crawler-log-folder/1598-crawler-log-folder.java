264f6796242ec1765f8ea44608751565b5f352fc
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2024-07-10 23:26:38 +0530

    Time: 2 ms (44.79%), Space: 41.8 MB (81.60%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -1,6 +1,6 @@
 class Solution {
     public int minOperations(String[] logs) {
-        String entry = "entry";
+        String entry = "dummy";
         Stack<String> st = new Stack<>();
         for(String s : logs) {
             if(s.equals("../")) {
