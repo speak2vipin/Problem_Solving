b8c6e13d5e829712342dca266acbfe15f2c94461
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2024-08-04 17:51:35 +0530

    Time: 190 ms (14.86%), Space: 72.8 MB (5.40%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
@@ -3,14 +3,13 @@ class Solution {
         int mod = 1000_000_007;
         List<Long> sumList = new ArrayList<>();
         for(int i=1; i<n+1; i++) {
-            for(int j=i; j<n+1; j++) {
-                long sum = 0l;
-                for(int k=i; k<=j; k++) {
-                    sum = (sum + nums[k-1])%mod;
-                }
+            long sum = 0l;
+            for(int j=i; j<=n; j++) {
+                sum = (sum + nums[j-1])%mod;
                 sumList.add(sum);
             }
         }
+        
         Collections.sort(sumList, (x,y)->Long.compare(x, y));
         long res = 0;
         for(int i=left-1; i<right; i++) {
