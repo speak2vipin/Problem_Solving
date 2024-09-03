b3c800713ae985f0325cea3f336265a3fd212d04
Vipin Kumar Gupta <65086274+speak2vipin@users.noreply.github.com>
2024-09-03 18:29:24 +0530

    Time: 73 ms (44.11%), Space: 45.7 MB (25.86%) - LeetHub

--- 3276-select-cells-in-grid-with-maximum-score/3276-select-cells-in-grid-with-maximum-score.java
+++ 3276-select-cells-in-grid-with-maximum-score/3276-select-cells-in-grid-with-maximum-score.java
@@ -1,5 +1,10 @@
 class Solution {
+    // With grid.length = 1 to 10
+    // So max numbers can 2^10 i.e. 1024
+    // maximum grid[i][j] = 100
+    // So dp = new int[101][1025];
     int dp[][] = new int[101][1025];
+    
     public int maxScore(List<List<Integer>> grid) {
         Map<Integer, ArrayList<Integer>> arr = new HashMap<>();
         for(int i=0; i<grid.size(); i++) {
@@ -11,8 +16,6 @@ class Solution {
             Arrays.fill(d, -1);
         }
         return solve(arr, 100, 0);
-        
-        
     }
     
     int solve(Map<Integer, ArrayList<Integer>> arr, int idx, int mask) {
@@ -24,6 +27,7 @@ class Solution {
         }
         int res = solve(arr, idx-1, mask);
         for(int i : arr.getOrDefault(idx, new ArrayList<>())) {
+            // Bit masking is basically have done to track whether a row has already been considered or not
             if((mask & (1<<i))==0) {
                res = Math.max(res, idx + solve(arr, idx-1, mask | (1<<i)));
             }
